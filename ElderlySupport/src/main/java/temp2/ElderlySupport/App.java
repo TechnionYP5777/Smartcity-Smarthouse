dc6f1e61242ee6f73244e3c1f9835440096ddbb0
Sharon Kuninin Levenzon <sharonkl@campus.technion.ac.il>
2016-11-22 08:53:08 -0800

    Re-initialize maven project with correct structure

---
+++ ElderlySupport/src/main/java/temp2/ElderlySupport/App.java
@@ -0,0 +1,13 @@
+package temp2.ElderlySupport;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
