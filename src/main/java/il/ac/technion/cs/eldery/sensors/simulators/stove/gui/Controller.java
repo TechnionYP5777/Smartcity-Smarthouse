c6cd4b04b40b40035a0ddbb20257408c328cc052
Sharon Kuninin Levenzon <sharonkl@campus.technion.ac.il>
2016-12-09 04:57:42 -0800

    Add fields for the stove GUI controller

--- src/main/java/il/ac/technion/cs/eldery/sensors/simulators/stove/gui/Controller.java
+++ src/main/java/il/ac/technion/cs/eldery/sensors/simulators/stove/gui/Controller.java
@@ -3,9 +3,20 @@ package il.ac.technion.cs.eldery.sensors.simulators.stove.gui;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.Slider;
 
 public class Controller implements Initializable {
+  @FXML
+  public Button onOffButton;
+  @FXML
+  public Label tempLabel;
+  @FXML
+  public Slider tempSlider;
+  
   @Override public void initialize(URL location, ResourceBundle __) {
     // TODO: Sharon implement controller and add XML javafx file
   }
