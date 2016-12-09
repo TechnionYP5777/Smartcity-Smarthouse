f026e65f587d3db353e6fec0202867e823076ca7
Sharon Kuninin Levenzon <sharonkl@campus.technion.ac.il>
2016-12-08 17:45:17 -0800

    Add the controller for the stove sensor simulator GUI
    Will be implemented in next commits.

---
+++ src/main/java/il/ac/technion/cs/eldery/sensors/simulators/stove/gui/Controller.java
@@ -0,0 +1,12 @@
+package il.ac.technion.cs.eldery.sensors.simulators.stove.gui;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.fxml.Initializable;
+
+public class Controller implements Initializable {
+  @Override public void initialize(URL location, ResourceBundle __) {
+    // TODO: Sharon implement controller and add XML javafx file
+  }
+}
