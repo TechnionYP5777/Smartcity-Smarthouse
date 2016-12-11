4b98ce41bd2964494a2b7146f240e8533fa5c278
Sharon Kuninin Levenzon <sharonkl@campus.technion.ac.il>
2016-12-11 12:22:32 -0800

    Implement the UpdateMessage class and add a test #58

---
+++ src/main/java/il/ac/technion/cs/eldery/networking/messages/UpdateMessage.java
@@ -0,0 +1,29 @@
+package il.ac.technion.cs.eldery.networking.messages;
+
+import java.util.Map;
+
+import com.google.gson.JsonObject;
+
+import il.ac.technion.cs.eldery.sensors.Sensor;
+
+public class UpdateMessage implements Message {
+  Sensor sensor;
+  Map<Object, Object> data;
+
+  public UpdateMessage(Sensor sensor, Map<Object, Object> data) {
+    this.sensor = sensor;
+    this.data = data;
+  }
+
+  @Override public String toJson() {
+    JsonObject json = new JsonObject();
+    json.addProperty("type", MessageType.UPDATE + "");
+    json.addProperty("id", sensor.getId());
+    JsonObject dataJson = new JsonObject();
+    data.entrySet().forEach(entry -> {
+      dataJson.addProperty(entry.getKey() + "", entry.getValue() + "");
+    });
+    json.add("data", dataJson);
+    return json + "";
+  }
+}
---
+++ src/test/java/il/ac/technion/cs/eldery/networking/messages/UpdateMessageTest.java
@@ -0,0 +1,25 @@
+package il.ac.technion.cs.eldery.networking.messages;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import com.google.gson.JsonParser;
+
+import il.ac.technion.cs.eldery.sensors.simulators.stove.StoveSensor;
+
+@SuppressWarnings("static-method")
+public class UpdateMessageTest {
+  @Test public void basicUpdateMessageTest() {
+    StoveSensor sensor = new StoveSensor("Stove Sensor", "00:11:22:33:44:55");
+    Map<Object, Object> data = new HashMap<>();
+    data.put("on", Boolean.FALSE);
+    data.put("temperature", "100");
+    UpdateMessage message = new UpdateMessage(sensor, data);
+    JsonParser parser = new JsonParser();
+    Assert.assertEquals(parser.parse(message.toJson()),
+        parser.parse("{\"type\":\"UPDATE\", \"id\":\"00:11:22:33:44:55\", \"data\": {\"on\":\"false\", \"temperature\":\"100\"}}"));
+  }
+}
