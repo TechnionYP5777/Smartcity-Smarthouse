a7fc2f6c0db40243ab6722f04d05a9fec1abc9d0
Sharon Kuninin Levenzon <sharonkl@campus.technion.ac.il>
2016-12-10 18:54:53 -0800

    Add documentation and author, since tags #56

--- src/main/java/networking/messages/Message.java
+++ src/main/java/networking/messages/Message.java
@@ -1,5 +1,9 @@
 package networking.messages;
 
+/** @author Sharon
+ * @since 11.12.16 */
 public interface Message {
+  /** Converts the contents of this message into JSON format.
+   * @return JSON formatted string */
   String toJson();
 }
