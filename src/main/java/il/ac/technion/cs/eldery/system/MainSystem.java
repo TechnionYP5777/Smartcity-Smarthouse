9fd64b2c09d0dd1bcbba34ab42de008f5910ae90
Elia Traore <set@campus.technion.ac.il>
2016-12-12 21:26:50 +0200

    created the class as per #49

--- src/main/java/il/ac/technion/cs/eldery/system/MainSystem.java
+++ src/main/java/il/ac/technion/cs/eldery/system/MainSystem.java
@@ -1,6 +1,16 @@
 package il.ac.technion.cs.eldery.system;
 
-// TODO: Author of this class what is it supposed to do?
+
+
+/** hold the databases of the smart house, and allow sensors and applications to store and read information about the changes
+ *  in the environment
+ * */
 public class MainSystem {
-    public MainSystem() {}
+  /** API allowing smart house applications to register for information and notify on emergencies
+   * */
+  static class ApplicationHandler {
+  }
+  
+  ApplicationHandler applicationHandler = new ApplicationHandler();
+  public MainSystem() {}
 }
