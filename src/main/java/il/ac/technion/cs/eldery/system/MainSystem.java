5716da658914a0efaa5ff4ee84e49e1cb3088859
Elia Traore <set@campus.technion.ac.il>
2016-12-12 22:01:31 +0200

    added doc to the previously mentioned api methods (related issues: #26,#49)

--- src/main/java/il/ac/technion/cs/eldery/system/MainSystem.java
+++ src/main/java/il/ac/technion/cs/eldery/system/MainSystem.java
@@ -14,11 +14,21 @@ public class MainSystem {
   /** API allowing smart house applications to register for information and notify on emergencies
    * */
   static class ApplicationHandler {
+      /** Allows registration to a sensor. on update, the data will be given to the consumer for farther processing
+       * @param sensorCommercialName The name of sensor, agreed upon in an external platform
+       * @param notifyWhen A predicate that will be called every time the sensor updates the date. If it returns true the consumer will be called
+       * @param notifee A consumer that will receive the new data from the sensor
+       * */
       public static <L,R> Boolean registerToSensor(String sensorCommercialName, Predicate<Tuple<L,R>> notifyWhen, 
                               Consumer<List<Tuple<L,R>>> notifee){
           return Boolean.FALSE; //TODO: ELIA implement
       }
       
+      /** Allows registration to a sensor. on time, the sensor will be polled and the data will be given to the consumer for farther processing 
+       * @param sensorCommercialName The name of sensor, agreed upon in an external platform
+       * @param time the time when a polling is requested
+       * @param notifee A consumer that will receive the new data from the sensor
+       * */
       public static <L,R> Boolean registerToSensor(String sensorCommercialName, LocalTime time, 
                               Consumer<List<Tuple<L,R>>> notifee){
       return Boolean.FALSE; //TODO: ELIA implement
