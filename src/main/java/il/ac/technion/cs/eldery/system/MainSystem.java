1b616a22c5c19005e3f602c7a962172481c4f3a5
Elia Traore <set@campus.technion.ac.il>
2016-12-12 21:48:50 +0200

    added api methods for registeration to sensor updates, as defined in issue #26. all part of the class created in #49

--- src/main/java/il/ac/technion/cs/eldery/system/MainSystem.java
+++ src/main/java/il/ac/technion/cs/eldery/system/MainSystem.java
@@ -1,6 +1,11 @@
 package il.ac.technion.cs.eldery.system;
 
+import java.time.LocalTime;
+import java.util.List;
+import java.util.function.Consumer;
+import java.util.function.Predicate;
 
+import il.ac.technion.cs.eldery.utils.Tuple;
 
 /** hold the databases of the smart house, and allow sensors and applications to store and read information about the changes
  *  in the environment
@@ -9,6 +14,15 @@ public class MainSystem {
   /** API allowing smart house applications to register for information and notify on emergencies
    * */
   static class ApplicationHandler {
+      public static <L,R> Boolean registerToSensor(String sensorCommercialName, Predicate<Tuple<L,R>> notifyWhen, 
+                              Consumer<List<Tuple<L,R>>> notifee){
+          return Boolean.FALSE; //TODO: ELIA implement
+      }
+      
+      public static <L,R> Boolean registerToSensor(String sensorCommercialName, LocalTime time, 
+                              Consumer<List<Tuple<L,R>>> notifee){
+      return Boolean.FALSE; //TODO: ELIA implement
+      }
   }
   
   ApplicationHandler applicationHandler = new ApplicationHandler();
