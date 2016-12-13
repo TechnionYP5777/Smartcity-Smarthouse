1ddd7e0f94800f9dd529db9a85e3a0a1d056d363
Elia Traore <set@campus.technion.ac.il>
2016-12-13 13:24:39 +0200

    added polling option on sensors + doc

--- src/main/java/il/ac/technion/cs/eldery/system/MainSystem.java
+++ src/main/java/il/ac/technion/cs/eldery/system/MainSystem.java
@@ -2,6 +2,7 @@ package il.ac.technion.cs.eldery.system;
 
 import java.time.LocalTime;
 import java.util.List;
+import java.util.Optional;
 import java.util.function.Consumer;
 import java.util.function.Predicate;
 
@@ -18,20 +19,29 @@ public class MainSystem {
        * @param sensorCommercialName The name of sensor, agreed upon in an external platform
        * @param notifyWhen A predicate that will be called every time the sensor updates the date. If it returns true the consumer will be called
        * @param notifee A consumer that will receive the new data from the sensor
+       * @return True if the registration was successful, false otherwise
        * */
       public static <L,R> Boolean registerToSensor(String sensorCommercialName, Predicate<Tuple<L,R>> notifyWhen, 
-                              Consumer<List<Tuple<L,R>>> notifee){
+                              Consumer<Tuple<L,R>> notifee){
           return Boolean.FALSE; //TODO: ELIA implement
       }
       
       /** Allows registration to a sensor. on time, the sensor will be polled and the data will be given to the consumer for farther processing 
        * @param sensorCommercialName The name of sensor, agreed upon in an external platform
-       * @param time the time when a polling is requested
+       * @param t the time when a polling is requested
        * @param notifee A consumer that will receive the new data from the sensor
+       * @return True if the registration was successful, false otherwise
        * */
-      public static <L,R> Boolean registerToSensor(String sensorCommercialName, LocalTime time, 
-                              Consumer<List<Tuple<L,R>>> notifee){
-      return Boolean.FALSE; //TODO: ELIA implement
+      public static <L,R> Boolean registerToSensor(String sensorCommercialName, LocalTime t, Consumer<Tuple<L,R>> notifee){
+          return Boolean.FALSE; //TODO: ELIA implement
+      }
+      
+      /** request for the latest data received by a sensor
+       *  @param sensorCommercialName The name of sensor, agreed upon in an external platform
+       *  @return the latest data (or Optional.empty() if the query failed in any point)
+       * */
+      public static <L,R> Optional<Tuple<L,R>> querySensor(String sensorCommercialName){
+          return Optional.empty(); //TODO: ELIA implement
       }
   }
   
