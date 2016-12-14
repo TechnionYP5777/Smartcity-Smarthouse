dd7a141e9079a361b6d0295e4ab22fcf84fd5001
Sharon <sharonkl@campus.technion.ac.il>
2016-12-14 16:25:33 +0200

    Change SensorHandler to be a Runnable thread
    A basic functionality of opening a UDP port and listening on it has been added

--- src/main/java/il/ac/technion/cs/eldery/system/sensors/SensorHandler.java
+++ src/main/java/il/ac/technion/cs/eldery/system/sensors/SensorHandler.java
@@ -1,12 +1,34 @@
 package il.ac.technion.cs.eldery.system.sensors;
 
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.SocketException;
 import java.util.HashMap;
 import java.util.Map;
 
-public class SensorHandler {
+public class SensorHandler implements Runnable {
     @SuppressWarnings("rawtypes") private Map<String, SensorInfo> sensors = new HashMap<>();
 
     @SuppressWarnings("rawtypes") public Map<String, SensorInfo> getSensors() {
         return sensors;
+    }
+
+    @Override public void run() {
+        try {
+            DatagramSocket socket = new DatagramSocket(100);
+            byte[] buffer = new byte[2048];
+            
+            for (DatagramPacket packet = new DatagramPacket(buffer, buffer.length); true;) {
+                socket.receive(packet);
+                String message = new String(buffer, 0, packet.getLength());
+                
+                // TODO: Sharon, convert to message and handle accordingly
+                
+                packet.setLength(buffer.length);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
