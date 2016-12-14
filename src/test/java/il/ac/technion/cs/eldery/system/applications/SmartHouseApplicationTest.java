e33db8a440ff3ad5f0e3592eabedef61bec8713c
Ron Gatenio <ron.gatenio@gmail.com>
2016-12-14 23:23:15 +0200

    There is still nothing to test in SmartHouseApplication... so i removed problematicstuff from there

--- src/test/java/il/ac/technion/cs/eldery/system/applications/SmartHouseApplicationTest.java
+++ src/test/java/il/ac/technion/cs/eldery/system/applications/SmartHouseApplicationTest.java
@@ -8,40 +8,29 @@ import il.ac.technion.cs.eldery.system.*;
  * @author RON
  * @since 8.12.16 */
 public class SmartHouseApplicationTest {
-    //MockApp testApp;
+    // MockApp testApp;
     MainSystem mainSystem;
-    
-    public SmartHouseApplicationTest() {
-        System.out.println(getClass().getPackage().getName() + "FFFF");
-        // TODO Auto-generated constructor stub
-    }
-    
+
+    public SmartHouseApplicationTest() {}
+
     @Before public void createApplication() {
-        //testApp = new MockApp();
-        try {
-            System.out.println(getClass().getPackage().getName());
-            getClass().getClassLoader().loadClass("il.ac.technion.cs.eldery.system.applications.examples.MyTestApp");
-        } catch (ClassNotFoundException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
-        }
         mainSystem = new MainSystem();
     }
 
     @Test public void testSetMainSystem() {
-        //Assert.assertTrue(testApp.setMainSystemInstance(mainSystem));
+        // Assert.assertTrue(testApp.setMainSystemInstance(mainSystem));
     }
 
     @Test public void testOnInstall() {
-        //testApp.onLoad();
-        //Assert.assertEquals(testApp.testFeedbak, "onInstall");
+        // testApp.onLoad();
+        // Assert.assertEquals(testApp.testFeedbak, "onInstall");
     }
 
     @Test public void testCheckIfSensorExists() {
-        //Assert.assertTrue(testApp.checkIfSensorExists("id"));
+        // Assert.assertTrue(testApp.checkIfSensorExists("id"));
     }
 
     @Test public void testSubscribeSensorExists() {
-        //Assert.assertTrue(testApp.subscribeToSensor("id"));
+        // Assert.assertTrue(testApp.subscribeToSensor("id"));
     }
 }
