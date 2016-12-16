f7eb8679fd4b71adad28de5b72eb890e3ef72d77
Sharon <sharonkl@campus.technion.ac.il>
2016-12-16 16:10:52 +0200

    Add documentation and generalize fields type

--- src/main/java/il/ac/technion/cs/eldery/system/sensors/SensorInfo.java
+++ src/main/java/il/ac/technion/cs/eldery/system/sensors/SensorInfo.java
@@ -7,72 +7,84 @@ import java.util.function.Consumer;
 import il.ac.technion.cs.eldery.utils.Generator;
 import il.ac.technion.cs.eldery.utils.Table;
 
-/**
- * 
- * @author Inbal Zukerman
- * @since 16.12.2016
- */
-public class SensorInfo{
-    
+/** @author Inbal Zukerman
+ * @author Sharon
+ * @since 16.12.2016 */
+public class SensorInfo {
     private Table<Object, Object> information = new Table<>();
-    HashMap<String, Consumer<HashMap<Object, Object>>> listeners = new HashMap<>();
-    
-    /*
-     * Creates SensorInfo without information capacity limit
-     */
-    public SensorInfo() { }
-    
-    /**
-     * @param infoLimit - information capacity limit to enforce
-     */
+    private Map<String, Consumer<Table<Object, Object>>> listeners = new HashMap<>();
+
+    /** Creates SensorInfo without information capacity limit. */
+    public SensorInfo() {}
+
+    /** @param infoLimit - information capacity limit to enforce */
     public SensorInfo(final int infoLimit) {
-        information.changeMaxCapacity(infoLimit);
+        this.information.changeMaxCapacity(infoLimit);
     }
-    
-    private SensorInfo(final Table<Object, Object> information){
+
+    /** Initializes a new sensor info object with the given information.
+     * @param information observation the object will be initialized with */
+    private SensorInfo(final Table<Object, Object> information) {
         this.information = information;
     }
-    
-    
-    public String addListener(final Consumer<HashMap<Object, Object>> listener){
+
+    /** Adds a new listener to changes made to the observations table.
+     * @param listener listener to be invoked when changed are made
+     * @return id of the added listener */
+    public String addListener(final Consumer<Table<Object, Object>> listener) {
         final String $ = Generator.GenerateUniqueIDstring();
         listeners.put($, listener);
         return $;
     }
-    
-    public void removeListener(final String listenerId){
+
+    /** Removes a listener from the listeners list.
+     * @param listenerId id of the listener to remove */
+    public void removeListener(final String listenerId) {
         listeners.remove(listenerId);
     }
-    
-    public void addRecord(final HashMap<Object, Object> record){
+
+    /** Adds a new entry to the table of observations.
+     * @param record entry to add */
+    public void addRecord(final HashMap<Object, Object> record) {
         information.addEntry(record);
     }
-    
-    public int getIfoLimit(){
+
+    /** @return max capacity of the observations table */
+    public int getInfoLimit() {
         return information.getMaxCapacity();
     }
-    
-    public int getNumRecords(){
+
+    /** @return current size of the observations table */
+    public int getNumRecords() {
         return information.getCurrentCapacity();
     }
-    
-    public void changeInfoLimit(final int newLimit){
+
+    /** Sets a new limit to the observations table.
+     * @param newLimit new limit */
+    public void changeInfoLimit(final int newLimit) {
         information.changeMaxCapacity(newLimit);
     }
-    
-    public void unlimitInfo(){
+
+    /** Changes the max size of the table to be unlimited. */
+    public void unlimitInfo() {
         information.disableCapacityLimit();
     }
-    
-    public SensorInfo getLastKRecords (final int k){
+
+    /** Returns a sensor info object containing only the last k observations.
+     * @param k amount of observations that remain
+     * @return new sensor info object */
+    public SensorInfo getLastKRecords(final int k) {
         return new SensorInfo(information.receiveKLastEntries(k));
     }
-    
-    public Map<Object, Object> getLastRecord(){
+
+    /** @return last observations entry */
+    public Map<Object, Object> getLastRecord() {
         return information.getLastEntry();
     }
-   
-    public Object getLastRecordAtCol(final Object colName){
+
+    /** @param colName observation name
+     * @return last observation of the given type */
+    public Object getLastRecordAtCol(final Object colName) {
         return information.getLastEntryAtCol(colName);
     }
 }
