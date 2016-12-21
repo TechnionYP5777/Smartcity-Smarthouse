21e95802f3a5c522151956c0e824cd005016dcd7
Sharon <sharonkl@campus.technion.ac.il>
2016-12-21 04:13:49 +0200

    Add console to the stove sensor simulator
    The console shows what messages are sent to the system.

--- src/main/java/il/ac/technion/cs/eldery/sensors/stove/gui/Controller.java
+++ src/main/java/il/ac/technion/cs/eldery/sensors/stove/gui/Controller.java
@@ -9,6 +9,7 @@ import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.Slider;
+import javafx.scene.control.TextArea;
 
 /** @author Sharon
  * @since 9.12.16 */
@@ -18,8 +19,9 @@ public class Controller implements Initializable {
     @FXML public Button onOffButton;
     @FXML public Label tempLabel;
     @FXML public Slider tempSlider;
+    @FXML public TextArea console;
 
-    @Override public void initialize(final URL location, final ResourceBundle __) {
+    @SuppressWarnings("boxing") @Override public void initialize(final URL location, final ResourceBundle __) {
         sensor = new StoveSensor("Stove Sensor Simulator", "00:00:00:00:00:00", "1:1:1:1", 80);
         // for (boolean res = false; !res;)
         // res = sensor.register();
@@ -28,11 +30,15 @@ public class Controller implements Initializable {
             onOffButton.setText("Turn " + (on ? "off" : "on"));
             tempLabel.setDisable(!on);
             tempSlider.setDisable(!on);
+            console.setText(
+                    String.format("Sending update message: {on: %1$b, temperature: %2$d}\n", on, (int) tempSlider.getValue()) + console.getText());
             sensor.updateSystem(on, (int) Math.round(tempSlider.getValue()));
         });
         tempSlider.valueProperty().addListener((ov, oldVal, newVal) -> {
             final int temp = (int) Math.round(newVal.doubleValue());
             tempLabel.setText("Temperature: " + temp);
+            console.setText(
+                    String.format("Sending update message: {on: %1$b, temperature: %2$d}\n", on, (int) tempSlider.getValue()) + console.getText());
             sensor.updateSystem(true, temp);
         });
     }
