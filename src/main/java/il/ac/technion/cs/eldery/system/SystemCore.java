304c4bf910dac79a7c1cb567222e0313289432f2
Sharon <sharonkl@campus.technion.ac.il>
2016-12-22 13:22:23 +0200

    Add needed suppress warnings to clean project from warnings

--- src/main/java/il/ac/technion/cs/eldery/system/SystemCore.java
+++ src/main/java/il/ac/technion/cs/eldery/system/SystemCore.java
@@ -5,6 +5,7 @@ import il.ac.technion.cs.eldery.system.sensors.SensorsHandler;
 
 /** Hold the databases of the smart house, and allow sensors and applications to
  * store and read information about the changes in the environment */
+@SuppressWarnings("unused")
 public class SystemCore {
     private final DatabaseHandler databaseHandler = new DatabaseHandler();
     private final SensorsHandler sensorsHandler = new SensorsHandler(databaseHandler);
