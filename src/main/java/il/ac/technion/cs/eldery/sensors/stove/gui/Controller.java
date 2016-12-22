23902553f9a69ba88295c5530b697dcf726d60aa
Sharon <sharonkl@campus.technion.ac.il>
2016-12-22 03:25:40 +0200

    Improve console and change to better looking colors

--- src/main/java/il/ac/technion/cs/eldery/sensors/stove/gui/Controller.java
+++ src/main/java/il/ac/technion/cs/eldery/sensors/stove/gui/Controller.java
@@ -4,24 +4,34 @@ import java.net.URL;
 import java.util.ResourceBundle;
 
 import il.ac.technion.cs.eldery.sensors.stove.StoveSensor;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.Slider;
 import javafx.scene.control.TextArea;
+import javafx.scene.text.Text;
+import javafx.scene.text.TextBuilder;
+import javafx.scene.text.TextFlow;
 
 /** @author Sharon
  * @since 9.12.16 */
 public class Controller implements Initializable {
+    private static final String STYLE_REG = "-fx-fill: #4d4d4c; -fx-font-weight: bold;";
+    private static final String STYLE_ON = "-fx-fill: #718c00; -fx-font-weight: bold;";
+    private static final String STYLE_OFF = "-fx-fill: #c82829; -fx-font-weight: bold;";
+    private static final String STYLE_TEMP = "-fx-fill: #4271ae; -fx-font-weight: bold;";
+
     private StoveSensor sensor;
     private boolean on;
     @FXML public Button onOffButton;
     @FXML public Label tempLabel;
     @FXML public Slider tempSlider;
-    @FXML public TextArea console;
+    @FXML public TextFlow console;
 
-    @SuppressWarnings("boxing") @Override public void initialize(final URL location, final ResourceBundle __) {
+    @Override public void initialize(final URL location, final ResourceBundle __) {
         sensor = new StoveSensor("Stove Sensor Simulator", "00:00:00:00:00:00", "1:1:1:1", 80);
         // for (boolean res = false; !res;)
         // res = sensor.register();
@@ -30,16 +40,41 @@ public class Controller implements Initializable {
             onOffButton.setText("Turn " + (on ? "off" : "on"));
             tempLabel.setDisable(!on);
             tempSlider.setDisable(!on);
-            console.setText(
-                    String.format("Sending update message: {on: %1$b, temperature: %2$d}\n", on, (int) tempSlider.getValue()) + console.getText());
             sensor.updateSystem(on, (int) Math.round(tempSlider.getValue()));
+            printUpdateMessage();
         });
         tempSlider.valueProperty().addListener((ov, oldVal, newVal) -> {
+            if (Math.round(oldVal.doubleValue()) == Math.round(newVal.doubleValue()))
+                return;
+
             final int temp = (int) Math.round(newVal.doubleValue());
             tempLabel.setText("Temperature: " + temp);
-            console.setText(
-                    String.format("Sending update message: {on: %1$b, temperature: %2$d}\n", on, (int) tempSlider.getValue()) + console.getText());
             sensor.updateSystem(true, temp);
+            printUpdateMessage();
         });
+    }
+
+    private void printUpdateMessage() {
+        ObservableList<Node> children = console.getChildren();
+
+        Text text = new Text("Sending update message: {on: ");
+        text.setStyle(STYLE_REG);
+        children.add(0, text);
+
+        text = new Text(on + "");
+        text.setStyle(on ? STYLE_ON : STYLE_OFF);
+        children.add(1, text);
+
+        text = new Text(", temperature: ");
+        text.setStyle(STYLE_REG);
+        children.add(2, text);
+
+        text = new Text((int) tempSlider.getValue() + "");
+        text.setStyle(STYLE_TEMP);
+        children.add(3, text);
+
+        text = new Text("}\n");
+        text.setStyle(STYLE_REG);
+        children.add(4, text);
     }
 }
--- src/main/java/il/ac/technion/cs/eldery/sensors/stove/gui/StoveSensorSimulator.java
+++ src/main/java/il/ac/technion/cs/eldery/sensors/stove/gui/StoveSensorSimulator.java
@@ -4,6 +4,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.text.TextFlow;
 import javafx.stage.Stage;
 
 /** @author Sharon
