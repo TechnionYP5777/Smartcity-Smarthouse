eea53858da559839cac2684e1fc56f1c4e9dea10
Yarden Lev <ylev@campus.technion.ac.il>
2016-12-23 17:26:19 -0800

    Change server to use TCP instead of UDP #83
    Currently, only one client is supported (will be changed soon).

--- src/main/java/il/ac/technion/cs/eldery/system/sensors/SensorsHandler.java
+++ src/main/java/il/ac/technion/cs/eldery/system/sensors/SensorsHandler.java
@@ -1,8 +1,11 @@
 package il.ac.technion.cs.eldery.system.sensors;
 
+import java.io.BufferedReader;
 import java.io.IOException;
-import java.net.DatagramPacket;
-import java.net.DatagramSocket;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
 
 import il.ac.technion.cs.eldery.networking.messages.AnswerMessage;
 import il.ac.technion.cs.eldery.networking.messages.AnswerMessage.Answer;
@@ -17,6 +20,7 @@ import il.ac.technion.cs.eldery.system.exceptions.SensorNotFoundException;
  * from sensors. The class can parse the different message and handle them
  * accordingly.
  * @author Sharon
+ * @author Yarden
  * @since 17.12.16 */
 public class SensorsHandler implements Runnable {
     private final DatabaseHandler databaseHandler;
@@ -28,34 +32,37 @@ public class SensorsHandler implements Runnable {
     }
 
     @Override public void run() {
-        try (DatagramSocket socket = new DatagramSocket(40000)) {
-            for (final DatagramPacket packet = new DatagramPacket(new byte[2048], new byte[2048].length);;) {
-                socket.receive(packet);
-                final Message message = MessageFactory.create(new String(packet.getData(), 0, packet.getLength()));
+        try (ServerSocket server = new ServerSocket(40000);
+                Socket client = server.accept();
+                PrintWriter out = new PrintWriter(client.getOutputStream(), true);
+                BufferedReader in = new BufferedReader(new InputStreamReader(client.getInputStream()));) {
+            for (String input = in.readLine(); input != null;) {
+                final Message message = MessageFactory.create(input);
                 if (message == null) {
-                    new AnswerMessage(Answer.FAILURE).send(packet.getAddress().getHostAddress(), packet.getPort());
+                    out.println(new AnswerMessage(Answer.FAILURE).toJson());
                     continue;
                 }
                 System.out.println("Received message: " + message + "\n");
                 switch (message.getType()) {
                     case REGISTRATION:
-                        handleRegisterMessage(packet, (RegisterMessage) message);
+                        handleRegisterMessage(out, (RegisterMessage) message);
                         break;
                     case UPDATE:
                         handleUpdateMessage((UpdateMessage) message);
                         break;
                     default:
                 }
+                input = in.readLine();
             }
         } catch (final IOException ¢) {
             ¢.printStackTrace();
         }
     }
 
-    private void handleRegisterMessage(final DatagramPacket p, final RegisterMessage ¢) {
+    private void handleRegisterMessage(PrintWriter out, final RegisterMessage ¢) {
         databaseHandler.addSensor(¢.getSensor().getId(), ¢.getSensor().getCommName(), 100);
 
-        new AnswerMessage(Answer.SUCCESS).send(p.getAddress().getHostAddress(), p.getPort());
+        out.println(new AnswerMessage(Answer.SUCCESS).toJson());
     }
 
     private void handleUpdateMessage(final UpdateMessage m) {
