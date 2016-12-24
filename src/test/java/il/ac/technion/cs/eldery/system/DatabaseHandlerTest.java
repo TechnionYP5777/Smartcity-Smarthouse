ba56ab8785680b9e3e4b8941469d1e57d9668f39
Sharon <sharonkl@campus.technion.ac.il>
2016-12-24 14:10:57 +0200

    Add tests for the DatabaseHandler class
    More tests were added, some using Mockito.

--- src/test/java/il/ac/technion/cs/eldery/system/DatabaseHandlerTest.java
+++ src/test/java/il/ac/technion/cs/eldery/system/DatabaseHandlerTest.java
@@ -2,10 +2,16 @@ package il.ac.technion.cs.eldery.system;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.function.Consumer;
 
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
+import org.mockito.Matchers;
+import org.mockito.Mockito;
+
+import il.ac.technion.cs.eldery.system.exceptions.SensorNotFoundException;
+import il.ac.technion.cs.eldery.utils.Table;
 
 public class DatabaseHandlerTest {
     private DatabaseHandler handler;
@@ -54,5 +60,47 @@ public class DatabaseHandlerTest {
         Assert.assertEquals(3, handler.getSensors("hiney").size());
         Assert.assertEquals(new ArrayList<>(Arrays.asList("33:11:22:33:44:55", "44:11:22:33:44:55", "55:11:22:33:44:55")),
                 handler.getSensors("hiney"));
+    }
+
+    @Test(expected = SensorNotFoundException.class) public void throwsExceptionWhenAddingListenerToNonExistingSensor()
+            throws SensorNotFoundException {
+        handler.addListener("Some id", null);
+    }
+
+    @Test public void addListenerToExistingSensorWithoutException() throws SensorNotFoundException {
+        handler.addSensor("00:11:22:33:44:55", "iStoves", 100);
+        handler.addListener("00:11:22:33:44:55", null);
+    }
+
+    @Test(expected = SensorNotFoundException.class) public void throwsExceptionWhenGettingTableForNonExistingSensor() throws SensorNotFoundException {
+        handler.getTable("Some id");
+    }
+
+    @Test public void getTableOfExistingSensorWithoutException() throws SensorNotFoundException {
+        handler.addSensor("00:11:22:33:44:55", "iStoves", 100);
+        handler.getTable("00:11:22:33:44:55");
+    }
+
+    @Test public void listenerIsCalledWhenAddingEntryToSensorTable() throws SensorNotFoundException {
+        @SuppressWarnings("unchecked") Consumer<Table<String, String>> listener = Mockito.mock(Consumer.class);
+
+        handler.addSensor("00:22:44:66:88:00", "iStoves", 100);
+        handler.addListener("00:22:44:66:88:00", listener);
+        handler.getTable("00:22:44:66:88:00").addEntry(null);
+
+        Mockito.verify(listener, Mockito.times(1)).accept(Matchers.any());
+    }
+
+    @Test public void listenerIsCalledMultipleTimesWhenAddingMultipleEntries() throws SensorNotFoundException {
+        @SuppressWarnings("unchecked") Consumer<Table<String, String>> listener = Mockito.mock(Consumer.class);
+
+        handler.addSensor("00:22:44:66:88:00", "iStoves", 100);
+        handler.addListener("00:22:44:66:88:00", listener);
+        handler.getTable("00:22:44:66:88:00").addEntry(null);
+        handler.getTable("00:22:44:66:88:00").addEntry(null);
+        handler.getTable("00:22:44:66:88:00").addEntry(null);
+        handler.getTable("00:22:44:66:88:00").addEntry(null);
+
+        Mockito.verify(listener, Mockito.times(4)).accept(Matchers.any());
     }
 }
