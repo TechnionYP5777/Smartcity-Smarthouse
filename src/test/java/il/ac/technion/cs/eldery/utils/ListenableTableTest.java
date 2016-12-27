c2adbf16f8b9115cee0ce67a1098896f40c47797
inbalzukerman <inbal.zukerman@gmail.com>
2016-12-28 00:07:36 +0200

    Tests added to ListenableTableTest and code coverage of ListenableTable now is 100%! This closes #94

--- src/test/java/il/ac/technion/cs/eldery/utils/ListenableTableTest.java
+++ src/test/java/il/ac/technion/cs/eldery/utils/ListenableTableTest.java
@@ -1,41 +1,54 @@
 package il.ac.technion.cs.eldery.utils;
 
-import org.junit.*;
-
 import java.util.HashMap;
 import java.util.Map;
 import java.util.function.Consumer;
+
+import org.junit.Assert;
+import org.junit.Test;
 
 /** @author Inbal Zukerman
  * @since Dec 27, 2016 */
 
 public class ListenableTableTest {
 
-    private ListenableTable<String, Integer> data = new ListenableTable<>();
+    private final ListenableTable<String, Integer> unlimitedTable = new ListenableTable<>();
+    private final ListenableTable<String, Integer> limitedTable = new ListenableTable<>(3);
     private final Map<String, Integer> info1 = new HashMap<>();
     private final Map<String, Integer> info2 = new HashMap<>();
 
     private boolean listenerUpdated;
 
-    private Consumer<Table<String, Integer>> listener = (x) -> {
+    private final Consumer<Table<String, Integer>> listener = (x) -> {
         listenerUpdated = true;
     };
 
+    @Test public void initalizationTest() {
+        Assert.assertEquals(Table.UNLIMITED_CAPACITY, unlimitedTable.getMaxCapacity());
+        Assert.assertEquals(3, limitedTable.getMaxCapacity());
+    }
+
     @Test public void listenersTest() {
-        String listenerId = this.data.addListener(listener);
+        final String listenerId = unlimitedTable.addListener(listener);
         Assert.assertNotNull(listenerId);
-        this.data.removeListener(listenerId);
-        Assert.assertNotNull(data);
+        unlimitedTable.removeListener(listenerId);
+        Assert.assertNotNull(unlimitedTable);
     }
 
     @Test public void addEntryTest() {
-        Assert.assertNull(this.data.getLastEntry());
-        this.data.addEntry(info1);
-        Assert.assertEquals(info1, this.data.getLastEntry());
+        Assert.assertNull(unlimitedTable.getLastEntry());
+        unlimitedTable.addEntry(info1);
+        Assert.assertEquals(info1, unlimitedTable.getLastEntry());
 
         Assert.assertFalse(listenerUpdated);
-        String listenerId = this.data.addListener(listener);
-        this.data.addEntry(info2);
+        unlimitedTable.addListener(listener);
+        unlimitedTable.addEntry(info2);
+        Assert.assertTrue(listenerUpdated);
+
+        listenerUpdated = false;
+        Assert.assertFalse(listenerUpdated);
+        limitedTable.addListener(listener);
+        limitedTable.addEntry(info1);
         Assert.assertTrue(listenerUpdated);
 
     }
