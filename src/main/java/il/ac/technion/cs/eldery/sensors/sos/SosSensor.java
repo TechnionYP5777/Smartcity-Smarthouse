af75e6e10ae09013e92baf9e8ff2379376c9c91f
Yarden Lev <ylev@campus.technion.ac.il>
2016-12-28 06:50:54 -0800

    Add a new class for the SOS sensor #27

---
+++ src/main/java/il/ac/technion/cs/eldery/sensors/sos/SosSensor.java
@@ -0,0 +1,33 @@
+package il.ac.technion.cs.eldery.sensors.sos;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import il.ac.technion.cs.eldery.sensors.Sensor;
+
+/** @author Yarden
+ * @since 28.12.16 */
+public class SosSensor extends Sensor {
+    private static final List<String> TYPES = new ArrayList<>();
+
+    static {
+        TYPES.add("SOS");
+    }
+
+    public SosSensor(final String id, final String commName, final String systemIP, final int systemPort) {
+        super(id, commName, TYPES, systemIP, systemPort);
+    }
+
+    public void updateSystem() {
+        final Map<String, String> data = new HashMap<>();
+        data.put("pressed", true + "");
+        super.updateSystem(data);
+    }
+
+    @Override public String[] getObservationsNames() {
+        return new String[] { "pressed" };
+    }
+
+}
--- src/main/java/il/ac/technion/cs/eldery/system/sensors/SensorsHandlerThread.java
+++ src/main/java/il/ac/technion/cs/eldery/system/sensors/SensorsHandlerThread.java
@@ -5,8 +5,6 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.Socket;
-import java.security.KeyStore.Entry;
-
 import com.google.gson.JsonObject;
 
 import il.ac.technion.cs.eldery.networking.messages.AnswerMessage;
