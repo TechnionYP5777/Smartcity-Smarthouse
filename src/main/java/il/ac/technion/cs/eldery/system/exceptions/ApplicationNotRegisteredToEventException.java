1b8ff9b86a709c7c2c632cf175969802b294cdea
Elia Traore <set@campus.technion.ac.il>
2016-12-28 05:22:42 +0200

    refactor the exceptions for clearity or sheker-colsheo

---
+++ src/main/java/il/ac/technion/cs/eldery/system/exceptions/ApplicationNotRegisteredToEventException.java
@@ -0,0 +1,13 @@
+/**
+ * 
+ */
+package il.ac.technion.cs.eldery.system.exceptions;
+
+/** @author Elia
+ * @since Dec 11, 2016 */
+public class ApplicationNotRegisteredToEventException extends Exception {
+    /**
+     * 
+     */
+    private static final long serialVersionUID = 1L;
+}
--- src/main/java/il/ac/technion/cs/eldery/system/exceptions/FailedApplicationInitializationException.java
+++ src/main/java/il/ac/technion/cs/eldery/system/exceptions/FailedApplicationInitializationException.java
@@ -3,11 +3,14 @@
  */
 package il.ac.technion.cs.eldery.system.exceptions;
 
-/** @author Elia
- * @since Dec 11, 2016 */
-public class ApplicationNotRegisteredToEvent extends Exception {
+/**
+ * @author Elia Traore
+ * @since Dec 28, 2016
+ */
+public class FailedApplicationInitializationException extends Exception {
     /**
      * 
      */
     private static final long serialVersionUID = 1L;
+
 }
