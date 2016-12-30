bbbbde1eabeef50c12f8b4758789c41509d04588
Ron Gatenio <ron.gatenio@gmail.com>
2016-12-31 01:29:03 +0200

    added javafx support to the SystemCore

--- src/main/java/il/ac/technion/cs/eldery/system/SystemCore.java
+++ src/main/java/il/ac/technion/cs/eldery/system/SystemCore.java
@@ -1,12 +1,27 @@
 package il.ac.technion.cs.eldery.system;
 
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+
+import il.ac.technion.cs.eldery.applications.sos.SosAppGui;
 import il.ac.technion.cs.eldery.system.applications.ApplicationsHandler;
+import il.ac.technion.cs.eldery.system.applications.installer.ApplicationPath;
+import il.ac.technion.cs.eldery.system.applications.installer.ApplicationPath.PathType;
+import il.ac.technion.cs.eldery.system.exceptions.AppInstallerException;
 import il.ac.technion.cs.eldery.system.sensors.SensorsHandler;
+import javafx.application.Application;
+import javafx.application.Platform;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
 /** Hold the databases of the smart house, and allow sensors and applications to
  * store and read information about the changes in the environment */
 @SuppressWarnings("unused")
-public class SystemCore {
+public class SystemCore extends Application {
     private final DatabaseHandler databaseHandler = new DatabaseHandler();
     private final SensorsHandler sensorsHandler = new SensorsHandler(databaseHandler);
     private final ApplicationsHandler applicationsHandler = new ApplicationsHandler(databaseHandler);
@@ -17,6 +32,10 @@ public class SystemCore {
     }
 
     public static void main(final String[] args) {
-        new SystemCore();
+        launch(args);
+    }
+
+    @Override public void start(Stage primaryStage) {
+        return;
     }
 }
