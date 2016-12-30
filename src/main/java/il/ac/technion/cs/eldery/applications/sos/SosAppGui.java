b729f6ac87dbe76b3d26f20373bef33e4e05a844
Ron Gatenio <ron.gatenio@gmail.com>
2016-12-30 03:02:17 +0200

    modified the sos app to extend smarthouseapplication

--- src/main/java/il/ac/technion/cs/eldery/applications/sos/SosAppGui.java
+++ src/main/java/il/ac/technion/cs/eldery/applications/sos/SosAppGui.java
@@ -1,22 +1,52 @@
 package il.ac.technion.cs.eldery.applications.sos;
 
-import javafx.application.Application;
+import il.ac.technion.cs.eldery.system.applications.api.SensorData;
+import il.ac.technion.cs.eldery.system.applications.api.SmartHouseApplication;
+import il.ac.technion.cs.eldery.system.exceptions.SensorNotFoundException;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-public class SosAppGui extends Application {
+public class SosAppGui extends SmartHouseApplication {
+    private SosController sosController;
+    
     public static void main(final String[] args) {
         launch(args);
     }
 
     @Override public void start(final Stage s) throws Exception {
-        final Parent root = FXMLLoader.load(getClass().getResource("sos_app_ui.fxml"));
-        final Scene scene = new Scene(root);
+        super.start(s);
+        final FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("sos_app_ui.fxml"));
+        final Parent root = fxmlLoader.load();
+        Scene scene = new Scene(root);
         s.setTitle("SOS Application");
         s.setScene(scene);
         s.setWidth(350);
         s.show();
+        
+        sosController = fxmlLoader.getController();
+    }
+
+    @Override public void onLoad() {
+        String sensorId = "00:00:00:00:00:01";
+        System.out.println("msg from app: onLoad");
+        try {
+            subscribeToSensor(sensorId, SosSensor.class, sosSensor -> {
+                String t = "SOS " + (sosSensor.isPressed() ? "" : "Not ") + "Pressed";
+                //sosController.stateLabel.setText(t);
+                System.out.println("msg from app: " + t);
+            });
+        } catch (SensorNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+}
+
+class SosSensor extends SensorData {
+    private boolean pressed;
+
+    boolean isPressed() {
+        return pressed;
     }
 }
