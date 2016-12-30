f965c2c276d1489baa632d11b5643d7bab4a98d2
inbalzukerman <inbal.zukerman@gmail.com>
2016-12-30 15:12:44 +0200

    Some modifications in the ContactTest class

--- src/test/java/il/ac/technion/cs/eldery/system/userInformation/ContactTest.java
+++ src/test/java/il/ac/technion/cs/eldery/system/userInformation/ContactTest.java
@@ -1,6 +1,7 @@
 package il.ac.technion.cs.eldery.system.userInformation;
 
 import org.junit.Test;
+import org.junit.Assert;
 
 /** @author Inbal Zukerman
  * @since Dec 28, 2016 */
@@ -10,20 +11,20 @@ public class ContactTest {
 
     @Test public void initializationTest() {
 
-        assert contactA.getId() == "123";
-        assert contactA.getName() == "Alon";
+        Assert.assertEquals(contactA.getId(), "123");
+        Assert.assertEquals(contactA.getName(), "Alon");
     }
 
     @Test public void phoneNumberTest() {
-        assert contactA.getPhoneNumber() == "0508080123";
+        Assert.assertEquals(contactA.getPhoneNumber(), "0508080123");
         contactA.setPhoneNumber("026798080");
-        assert contactA.getPhoneNumber() == "026798080";
+        Assert.assertEquals(contactA.getPhoneNumber(), "026798080");
     }
 
     @Test public void emailAddressTest() {
-        assert contactA.getEmailAddress() == "alon@gmail.com";
+        Assert.assertEquals(contactA.getEmailAddress(), "alon@gmail.com");
         contactA.setEmailAddress("alon100@gmail.com");
-        assert contactA.getEmailAddress() == "alon100@gmail.com";
+        Assert.assertEquals(contactA.getEmailAddress(), "alon100@gmail.com");
     }
 
 }
