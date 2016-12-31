e1fecef6f8e6b608f63fec65827c8ca730c4397f
Ron Gatenio <ron.gatenio@gmail.com>
2016-12-31 03:09:48 +0200

    added an exceptions package for the API

---
+++ src/main/java/il/ac/technion/cs/eldery/system/applications/api/exceptions/package-info.java
@@ -0,0 +1,4 @@
+/** A package for API related exceptions that can be thrown by the developer
+ * @author RON
+ * @since 31-12-2016 */
+package il.ac.technion.cs.eldery.system.applications.api.exceptions;
