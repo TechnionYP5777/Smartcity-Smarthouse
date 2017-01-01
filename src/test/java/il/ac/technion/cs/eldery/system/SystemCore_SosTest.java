86c23d756aa59a889b45f0e78813eb16759babfa
Ron Gatenio <ron.gatenio@gmail.com>
2017-01-01 21:25:34 +0200

    modified the SystemCore_SosTest

--- src/test/java/il/ac/technion/cs/eldery/system/SystemCore_SosTest.java
+++ src/test/java/il/ac/technion/cs/eldery/system/SystemCore_SosTest.java
@@ -4,31 +4,37 @@ import java.io.IOException;
 import java.util.Scanner;
 
 import il.ac.technion.cs.eldery.applications.sos.SosAppGui;
+import il.ac.technion.cs.eldery.sensors.sos.gui.SosSensorSimulator;
 import il.ac.technion.cs.eldery.system.applications.api.exceptions.OnLoadException;
 import il.ac.technion.cs.eldery.system.applications.installer.ApplicationPath;
 import il.ac.technion.cs.eldery.system.applications.installer.ApplicationPath.PathType;
 import il.ac.technion.cs.eldery.system.exceptions.AppInstallerException;
 import javafx.stage.Stage;
 
+/** This is an interactive test. Run it as a java application, and when
+ * requested, please open the SosSensorSimulator as a java application.
+ * @author RON
+ * @since 01-01-2017 */
 public class SystemCore_SosTest extends SystemCore {
     public static void main(final String args[]) {
         launch(args);
     }
 
     @SuppressWarnings("resource") @Override public void start(final Stage primaryStage) {
+        System.out.println("## System started!");
+
         while (true) {
-            System.out.println("## please start SosSensorSimulator, and press Enter:");
-            System.out.print(">>");
+            System.out.println("## please start SosSensorSimulator (" + SosSensorSimulator.class.getName() + "), and press Enter:");
             new Scanner(System.in).nextLine();
 
-            System.out.println("## Adding app...");
+            System.out.println("## Trying to add the app...");
             try {
                 applicationsHandler.addApplication("someID", new ApplicationPath<>(PathType.CLASS_NAME, SosAppGui.class.getName()));
-                System.out.println("## App added successfully!");
+                System.out.println("## App added and started successfully!");
                 break;
             } catch (AppInstallerException | IOException | OnLoadException e) {
-                System.out.println("## App installation faild: " + e.getMessage() + "\n");
-                // e.printStackTrace();
+                System.out.println("## App installation faild: " + e.getMessage());
+                System.out.println(String.valueOf(new char[80]).replace("\0", "#"));
             }
         }
     }
