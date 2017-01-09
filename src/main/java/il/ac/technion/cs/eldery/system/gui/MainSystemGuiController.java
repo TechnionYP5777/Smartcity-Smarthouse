5f74e521f255de45f2b80a44c2f2506327864d04
roy-shchory <royshch1@gmail.com>
2017-01-09 21:17:43 +0200

    added the main system gui classes and fxml files(start of implementaiton)

---
+++ src/main/java/il/ac/technion/cs/eldery/system/gui/MainSystemGuiController.java
@@ -0,0 +1,15 @@
+package il.ac.technion.cs.eldery.system.gui;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.fxml.Initializable;
+
+public class MainSystemGuiController implements Initializable{
+
+    @Override public void initialize(URL arg0, ResourceBundle arg1) {
+        // TODO Auto-generated method stub
+        
+    }
+
+}
