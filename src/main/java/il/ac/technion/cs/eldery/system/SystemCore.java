6b6be6c449c6b309afd61e2887e8b83b95cc3e3a
inbalzukerman <inbal.zukerman@gmail.com>
2017-01-16 19:13:35 +0200

    Some additions to SystemCore to connect the user information GUI to the backend logic #16

--- src/main/java/il/ac/technion/cs/eldery/system/SystemCore.java
+++ src/main/java/il/ac/technion/cs/eldery/system/SystemCore.java
@@ -5,9 +5,9 @@ import java.io.IOException;
 import il.ac.technion.cs.eldery.system.applications.ApplicationsHandler;
 import il.ac.technion.cs.eldery.system.gui.mapping.MappingController;
 import il.ac.technion.cs.eldery.system.sensors.SensorsHandler;
+import il.ac.technion.cs.eldery.system.userInformation.UserInformation;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
@@ -18,6 +18,8 @@ public class SystemCore extends Application {
     private final DatabaseHandler databaseHandler = new DatabaseHandler();
     private final SensorsHandler sensorsHandler = new SensorsHandler(databaseHandler);
     protected final ApplicationsHandler applicationsHandler = new ApplicationsHandler(databaseHandler);
+    private UserInformation user;
+    private boolean userInitialized;
 
     public static void main(final String[] args) {
         launch(args);
@@ -30,13 +32,27 @@ public class SystemCore extends Application {
         // TODO: Roy, add some GUI magic here
         // For now the system initializes the house mapping GUI, should be
         // changed in the future
-        FXMLLoader loader = new FXMLLoader(getClass().getResource("gui/mapping/house_mapping.fxml"));
-        
-        Scene scene = new Scene(loader.load(), 1000, 800);
+        final FXMLLoader loader = new FXMLLoader(getClass().getResource("gui/mapping/house_mapping.fxml"));
+
+        final Scene scene = new Scene(loader.load(), 1000, 800);
         s.setTitle("Test");
         s.setScene(scene);
         s.show();
-        
+
         ((MappingController) loader.getController()).setDatabaseHandler(databaseHandler);
     }
+
+    public UserInformation getUser() {
+        return user;
+    }
+
+    public void initializeUser(final String name, final String id, final String phoneNumber, final String homeAddress) {
+        user = new UserInformation(name, id, phoneNumber, homeAddress);
+        userInitialized = true;
+    }
+
+    public boolean isUserInitialized() {
+        return userInitialized;
+    }
+
 }
