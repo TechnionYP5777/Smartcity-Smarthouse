e578301a34f84714536e251ab6288bfe5d043eda
Elia Traore <set@campus.technion.ac.il>
2017-01-16 04:11:44 +0200

    cleaned up, format, laconzied

--- src/test/java/il/ac/technion/cs/eldery/system/CommunicateTest.java
+++ src/test/java/il/ac/technion/cs/eldery/system/CommunicateTest.java
@@ -3,24 +3,13 @@
  */
 package il.ac.technion.cs.eldery.system;
 
-import javax.mail.Authenticator;
-import javax.mail.PasswordAuthentication;
-
 import org.junit.Assert;
 import org.junit.Test;
 
-
-/**
- * @author Elia Traore
- * @since Jan 16, 2017
- */
+/** @author Elia Traore
+ * @since Jan 16, 2017 */
 public class CommunicateTest {
-    @Test public void sendEmailTest(){
-        String from = "smarthouse5777@gmail.com";
-        Assert.assertNotNull(Communicate.throughEmail(from, new Authenticator() {
-            protected PasswordAuthentication getPasswordAuthentication() {
-                return new PasswordAuthentication(from, "smart5house777");
-            }
-        }, "ron.gatenio@gmail.com", "ya bishhhh #worksss"));
+    @Test public void sendEmailFromHouseTest() {
+        Assert.assertNotNull(Communicate.throughEmailFromHere("ron.gatenio@gmail.com", "ya bishhhh #worksss"));
     }
 }
