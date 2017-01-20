7304753a98c92f15d5e39a72e2e9c8e4c09baf10
inbalzukerman <inbal.zukerman@gmail.com>
2017-01-20 15:32:47 +0200

    as agreed, deleting the ApplicationManagerTest class. This closes #102

--- src/test/java/il/ac/technion/cs/eldery/system/applications/ApplicationManagerTest.java
+++
@@ -1,9 +0,0 @@
-package il.ac.technion.cs.eldery.system.applications;
-
-public class ApplicationManagerTest {
-
-    public ApplicationManagerTest() {
-        // TODO Auto-generated constructor stub
-    }
-
-}
