bf92306af72e61ea89f6e25037f0c27f8230ab67
ylev\y_lev <ylev@campus.technion.ac.il>
2017-01-20 03:06:05 +0200

    Add a basic vitals application #37 #38
    The pulse will move to a different graph.

---
+++ src/test/java/il/ac/technion/cs/eldery/system/SystemCore_VitalsTest.java
@@ -0,0 +1,39 @@
+package il.ac.technion.cs.eldery.system;
+
+import java.io.IOException;
+import java.util.Scanner;
+
+import il.ac.technion.cs.eldery.applications.vitals.VitalsApp;
+import il.ac.technion.cs.eldery.sensors.vitals.gui.VitalsSensorSimulator;
+import il.ac.technion.cs.eldery.system.applications.api.exceptions.OnLoadException;
+import il.ac.technion.cs.eldery.system.applications.installer.ApplicationPath;
+import il.ac.technion.cs.eldery.system.applications.installer.ApplicationPath.PathType;
+import il.ac.technion.cs.eldery.system.exceptions.AppInstallerException;
+import javafx.stage.Stage;
+
+public class SystemCore_VitalsTest extends SystemCore {
+    public static void main(final String args[]) {
+        launch(args);
+    }
+
+    @Override @SuppressWarnings("resource") public void start(final Stage primaryStage) {
+        new Thread(sensorsHandler).start();
+        System.out.println("## System started!");
+
+        while (true) {
+            System.out.println("## please start VitalsSensorSimulator (" + VitalsSensorSimulator.class.getName() + "), and press Enter:");
+            new Scanner(System.in).nextLine();
+
+            System.out.println("## Trying to add the app...");
+            try {
+                applicationsHandler.addApplication("someID", new ApplicationPath<>(PathType.CLASS_NAME, VitalsApp.class.getName()));
+                System.out.println("## App added and started successfully!");
+                break;
+            } catch (AppInstallerException | IOException | OnLoadException ¢) {
+                System.out.println("## App installation failed: " + ¢.getMessage());
+                System.out.println(String.valueOf(new char[80]).replace("\0", "#"));
+            }
+        }
+    }
+
+}
