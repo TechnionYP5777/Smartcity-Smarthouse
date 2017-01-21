92510668de0b46ba5453a4e8bfa56bebccb0677c
roy-shchory <royshch1@gmail.com>
2017-01-22 01:28:01 +0200

    added browse files to system core

--- src/main/java/il/ac/technion/cs/eldery/system/gui/applications/ApplicationViewController.java
+++ src/main/java/il/ac/technion/cs/eldery/system/gui/applications/ApplicationViewController.java
@@ -1,5 +1,6 @@
 package il.ac.technion.cs.eldery.system.gui.applications;
 
+import java.io.File;
 import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -19,6 +20,8 @@ import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.ListView;
 import javafx.scene.layout.AnchorPane;
+import javafx.stage.FileChooser;
+import javafx.stage.Stage;
 
 public class ApplicationViewController implements Initializable {
     @FXML ListView<String> listView;
@@ -47,14 +50,10 @@ public class ApplicationViewController implements Initializable {
     
     private void initPlusBtn() {
         this.plusButton.setOnAction(e -> {
-            try {
-                appsHandler.addApplication(new ApplicationPath<>(PathType.CLASS_NAME, SosAppGui.class.getName()));
-                appsHandler.addApplication(new ApplicationPath<>(PathType.CLASS_NAME, StoveModuleGui.class.getName()));
-            } catch (AppInstallerException | IOException | OnLoadException e1) {
-                e1.printStackTrace();
-            }
-            
-            updateListView();
+            Stage stage=new Stage();
+            FileChooser fileChooser = new FileChooser();
+            fileChooser.setTitle("Open Resource File");
+            File file=fileChooser.showOpenDialog(stage);
         });
     }
     
