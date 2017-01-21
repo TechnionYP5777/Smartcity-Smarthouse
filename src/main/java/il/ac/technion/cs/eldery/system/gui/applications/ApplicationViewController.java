26d595722fc6e8ca3836ce1a3b6d41dfbbfc9f06
roy-shchory <royshch1@gmail.com>
2017-01-21 20:17:20 +0200

    main system gui changes for application loading

--- src/main/java/il/ac/technion/cs/eldery/system/gui/applications/ApplicationViewController.java
+++ src/main/java/il/ac/technion/cs/eldery/system/gui/applications/ApplicationViewController.java
@@ -2,21 +2,53 @@ package il.ac.technion.cs.eldery.system.gui.applications;
 
 import java.io.IOException;
 import java.net.URL;
+import java.util.Collection;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.ResourceBundle;
-
-import il.ac.technion.cs.eldery.system.DatabaseHandler;
-import il.ac.technion.cs.eldery.system.exceptions.SensorNotFoundException;
-import javafx.application.Platform;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
-import javafx.scene.layout.VBox;
+import javafx.scene.Node;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
+import javafx.scene.control.SplitPane;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.Pane;
 
 public class ApplicationViewController implements Initializable {
-
+    @FXML ListView<String> listView;
+    @FXML AnchorPane appView;
+    @FXML Button plusButton;
+    ObservableList<String> names;
     @Override public void initialize(URL location, ResourceBundle __) {
-
+        this.names = FXCollections.observableArrayList(
+                "Stove", "SOS");
+        listView.setItems(names);
+        listView.setOnMouseClicked(new EventHandler<MouseEvent>(){
+            @Override
+            public void handle(MouseEvent arg0) {
+                ObservableList<String> selected = listView.getSelectionModel().getSelectedItems();
+            }   
+            
+        });
+        this.plusButton.setOnAction(new EventHandler<ActionEvent>() {
+            
+            @Override public void handle(ActionEvent event) {
+                // TODO Auto-generated method stub
+            }
+        });
+    }
+    
+    public void addListItem(String name){
+        names.add(name);
+        listView.setItems(this.names);
     }
 }
