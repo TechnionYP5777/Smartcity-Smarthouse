1acf930ddabb7f048615544255ad30e5de89d2f0
Ron Gatenio <ron.gatenio@gmail.com>
2017-01-22 19:34:15 +0200

    added an enum for the premade apps

---
+++ src/main/java/il/ac/technion/cs/eldery/applications/PremadeApplications.java
@@ -0,0 +1,30 @@
+package il.ac.technion.cs.eldery.applications;
+
+import il.ac.technion.cs.eldery.applications.sos.SosAppGui;
+import il.ac.technion.cs.eldery.applications.stove.StoveModuleGui;
+import il.ac.technion.cs.eldery.applications.vitals.VitalsApp;
+import il.ac.technion.cs.eldery.system.applications.api.SmartHouseApplication;
+
+public enum PremadeApplications {
+    SOS_APPLICATION(SosAppGui.class, "SOS Application"),
+    STOVE_APPLICATION(StoveModuleGui.class, "Stove Application"),
+    VITALS_APPLICATION(VitalsApp.class, "Vitals Application")
+    ;
+    
+    private final Class<? extends SmartHouseApplication> appClass;
+    private final String appName;
+    
+    private PremadeApplications(final Class<? extends SmartHouseApplication> appClass, final String appName) {
+        this.appClass = appClass;
+        this.appName = appName;
+    }
+    
+    public Class<? extends SmartHouseApplication> getAppClass() {
+        return appClass;
+    }
+    
+    public String getAppName() {
+        return appName;
+    }
+}
+
