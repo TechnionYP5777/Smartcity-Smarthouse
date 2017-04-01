8cdb82b35144e63285f3a9fe68e2e37036c4d304
roy-shchory <royshch1@gmail.com>
2017-04-02 01:33:53 +0300

    still not there yet - removing till i can figure out how to not screw up the rest of the test

--- src/test/java/il/ac/technion/cs/eldery/guitesting/InstallTest.java
+++ src/test/java/il/ac/technion/cs/eldery/guitesting/InstallTest.java
@@ -1,30 +1,17 @@
 package il.ac.technion.cs.eldery.guitesting;
 
-import org.junit.BeforeClass;
-import org.junit.Test;
+
 import org.loadui.testfx.GuiTest;
-import org.loadui.testfx.utils.FXTestUtils;
 
 import il.ac.technion.cs.eldery.system.SystemCore;
 import javafx.scene.Parent;
 
-public class InstallTest{
-    private static GuiTest controller;
-    @BeforeClass
-    public static void setUpClass() {
-        FXTestUtils.launchApp(SystemCore.class);
-        controller = new GuiTest() {
-            @Override
-            protected Parent getRootNode() {
-                return SystemCore.getStage().getScene().getRoot();
-            }
-        };
-    }
+public class InstallTest extends GuiTest{
     
-    @SuppressWarnings("static-method")
-    @Test
-    public void verifyStuff(){
-        controller.click();
+    @Override protected Parent getRootNode() {
+        // TODO Auto-generated method stub
+        //SystemCore sysCore = new SystemCore();
+        return null;
     }
 
 }
