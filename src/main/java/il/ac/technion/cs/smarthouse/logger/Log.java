9bf428229d146d7b98b0555af2a0c7e177ddc2a3
Ron <ron.gatenio@gmail.com>
2017-04-22 16:20:25 +0300

    Added a global logger - not used for now

---
+++ src/main/java/il/ac/technion/cs/smarthouse/logger/Log.java
@@ -0,0 +1,9 @@
+package il.ac.technion.cs.smarthouse.logger;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public enum Log {
+    ;
+    public static final Logger log = LoggerFactory.getLogger("SmarthouseLogger");
+}
