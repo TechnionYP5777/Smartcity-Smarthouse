6c971153535ab37208e995b5106c2cd8fa9c6a4a
Ron Gatenio <ron.gatenio@gmail.com>
2017-04-24 00:14:22 +0300

    Started the work on the reorganization of the project #139

--- src/test/java/il/ac/technion/cs/smarthouse/system/applications/installer/examples/MyTestClass3.java
+++
@@ -1,7 +0,0 @@
-package il.ac.technion.cs.smarthouse.system.applications.installer.examples;
-
-public class MyTestClass3 {
-
-    public MyTestClass3() {}
-
-}
--- src/test/java/il/ac/technion/cs/smarthouse/system/services/Handler.java
+++
@@ -1,7 +0,0 @@
-package il.ac.technion.cs.smarthouse.system.services;
-
-/**
- * @author Elia Traore
- * @since Apr 1, 2017
- */
-public enum Handler {APPS, SENSORS, DB, SERVICES} //I hate you java. this DOES NOT justify its own file.
