5e1dd27c6e2d4a50914a1574b519d3b18953b338
Ron Gatenio <ron.gatenio@gmail.com>
2017-05-28 14:06:43 +0300

    Modified the FileSystem and added an interface for it

--- System/src/main/java/il/ac/technion/cs/smarthouse/system/file_system/FileSystem.java
+++ System/src/main/java/il/ac/technion/cs/smarthouse/system/file_system/FileSystem.java
@@ -8,87 +8,118 @@ import java.util.Collection;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Optional;
 import java.util.function.Consumer;
 
-import org.parse4j.ParseException;
-import org.parse4j.ParseObject;
-
-import il.ac.technion.cs.smarthouse.database.DatabaseAPI;
-import il.ac.technion.cs.smarthouse.system.Dispatcher;
-import il.ac.technion.cs.smarthouse.system.InfoType;
 import il.ac.technion.cs.smarthouse.system.dispatcher.PathBuilder;
-import il.ac.technion.cs.smarthouse.utils.Tuple;
 import il.ac.technion.cs.smarthouse.utils.UuidGenerator;
 
 /**
+ * An event handler file system implementation class.
+ * <P>
+ * This implementation uses a file tree for storing the data and eventHandlers
+ * 
  * @author RON
+ * @author Inbal Zukerman
  * @since 28-05-2017
  */
-public class FileSystem {
-    
+public class FileSystem implements IFileSystem {
+
     static class FileNode {
         private Object data;
         private Map<String, Consumer<String>> eventHandlers = new HashMap<>();
         private Map<String, FileNode> children = new HashMap<>();
-        
+
         @SuppressWarnings("unchecked")
         <T> T getData() {
-            return (T)data;
+            return (T) data;
         }
-        
+
         public void setData(Object data) {
             this.data = data;
         }
-        
-        Map<String, FileNode> getChildren() {
-            return children; //TODO: RON
+
+        public Collection<String> getChildrenNames() {
+            return children.keySet();
         }
-        
+
+        public FileNode getChild(String name, boolean create) {
+            return children.containsKey(name) ? children.get(name)
+                            : !create ? null : children.put(name, new FileNode());
+        }
+
         String addEventHandler(Consumer<String> eventHandler) {
             final String id = UuidGenerator.GenerateUniqueIDstring();
             eventHandlers.put(id, eventHandler);
             return id;
         }
-        
-        public Map<String, Consumer<String>> getEventHandlers() {
-            return eventHandlers;
+
+        void removeEventHandler(String eventHandlerId) {
+            eventHandlers.remove(eventHandlerId);
         }
-    }
-    
-    private FileNode root = new FileNode();
-    
-    private Tuple<FileNode, List<Consumer<String>>> getFileNodeByPath(String... path) {
-        List<Consumer<String>> eventHandlersOnBranch = new ArrayList<>();
-        
-        FileNode node = root;
-        eventHandlersOnBranch.addAll(node.getEventHandlers().values());
-        
-        for (String pathNode : PathBuilder.decomposePath(path)) {
-            node = node.getChildren().get(pathNode);
-            eventHandlersOnBranch.addAll(node.getEventHandlers().values());
+
+        public Collection<Consumer<String>> getEventHandlers() {
+            return eventHandlers.values();
         }
-        
-        return new Tuple<>(node, eventHandlersOnBranch);
-    }
-    
-    
-    public String subscribe(Consumer<String> eventHandler, String... path) {
-        return getFileNodeByPath(path).left.addEventHandler(eventHandler);
-    }
-    
-    public void unsubscribe(String eventHandlerId, String... path) {
-        getFileNodeByPath(path).left.getEventHandlers().remove(eventHandlerId);
-    }
-    
-    public void sendMessage(Object data, String... path) {
-        Tuple<FileNode, List<Consumer<String>>> t = getFileNodeByPath(path);
-        t.left.setData(data);
-        for (Consumer<String> eventHandler : t.right)
-            eventHandler.accept(data + "");//TODO: should be path+data; TODO: should be on a new thread later
-    }
-    
-    public <T> T getData(String... path) {
-        return getFileNodeByPath(path).left.getData();
     }
 
+    private class FileSystemWalkResults {
+        FileNode fileNode;
+        List<Consumer<String>> eventHandlersOnBranch;
+
+        public FileSystemWalkResults(FileNode fileNode, List<Consumer<String>> eventHandlersOnBranch) {
+            this.fileNode = fileNode;
+            this.eventHandlersOnBranch = eventHandlersOnBranch;
+        }
+    }
+
+    private FileNode root = new FileNode();
+
+    private FileSystemWalkResults fileSystemWalk(boolean create, String... path) {
+        List<Consumer<String>> eventHandlersOnBranch = new ArrayList<>();
+
+        FileNode node = root;
+        eventHandlersOnBranch.addAll(node.getEventHandlers());
+
+        for (String pathNode : PathBuilder.decomposePath(path)) {
+            node = node.getChild(pathNode, create);
+            if (node == null)
+                return new FileSystemWalkResults(null, eventHandlersOnBranch);
+            eventHandlersOnBranch.addAll(node.getEventHandlers());
+        }
+
+        return new FileSystemWalkResults(node, eventHandlersOnBranch);
+    }
+
+    @Override
+    public String subscribe(Consumer<String> eventHandler, String... path) {
+        return fileSystemWalk(true, path).fileNode.addEventHandler(eventHandler);
+    }
+
+    @Override
+    public void unsubscribe(String eventHandlerId, String... path) {
+        Optional.of(fileSystemWalk(false, path).fileNode).ifPresent(n -> n.removeEventHandler(eventHandlerId));
+    }
+
+    @Override
+    public void sendMessage(Object data, String... path) {
+        FileSystemWalkResults r = fileSystemWalk(true, path);
+
+        r.fileNode.setData(data);
+
+        for (Consumer<String> eventHandler : r.eventHandlersOnBranch)
+            eventHandler.accept(data + "");// TODO: should be path+data; TODO:
+                                           // should be on a new thread later
+    }
+
+    @Override
+    public <T> T getData(String... path) {
+        return fileSystemWalk(false, path).fileNode == null ? null : fileSystemWalk(false, path).fileNode.getData();
+    }
+
+    @Override
+    public Collection<String> getChildren(String... path) {
+        return fileSystemWalk(false, path).fileNode == null ? null
+                        : fileSystemWalk(false, path).fileNode.getChildrenNames();
+    }
 }
