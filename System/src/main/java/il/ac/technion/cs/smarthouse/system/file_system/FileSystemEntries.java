7708760bc0ed3e1ee395ef24f8848785fd0487cc
Ron Gatenio <ron.gatenio@gmail.com>
2017-05-29 01:08:11 +0300

    Added a file system layout idea to the comments in FileSystemEntries

--- System/src/main/java/il/ac/technion/cs/smarthouse/system/file_system/FileSystemEntries.java
+++ System/src/main/java/il/ac/technion/cs/smarthouse/system/file_system/FileSystemEntries.java
@@ -4,6 +4,7 @@ package il.ac.technion.cs.smarthouse.system.file_system;
  * An enum for all predefined entries in the file system / dispatcher
  * 
  * @author RON
+ * @author Inbal Zukerman
  * @since 28-05-2017
  */
 public enum FileSystemEntries {
@@ -14,7 +15,34 @@ public enum FileSystemEntries {
     SYSTEM_INTERNALS_SAVEME(PathBuilder.buildPath(SYSTEM_INTERNALS, "saveme")),
     SYSTEM_DATA_IMAGE(PathBuilder.buildPath(SYSTEM, "data_image")),
     APPLICATIONS_DATA("applications_data");
-
+    
+    /*
+     * File System Layout:
+     * 
+     *  /
+     *  ├───applications_data
+     *  ├───sensors
+     *  │   ├───COMMERCIAL_NAME_1
+     *  │   │   ├───SENSOR_ID_1
+     *  │   │   │   ├───done
+     *  │   │   │   └───location
+     *  │   │   └───SENSOR_ID_2
+     *  │   │       ├───done
+     *  │   │       └───location
+     *  │   └───COMMERCIAL_NAME_2
+     *  │       └───SENSOR_ID_3
+     *  │           ├───done
+     *  │           └───location
+     *  ├───sensors_data
+     *  │   └───DATA_FIELD_1
+     *  │       └───DATA_FIELD_2
+     *  │           └───SENSOR_ID_1
+     *  └───system
+     *      ├───data_image
+     *      ├───internals
+     *      └───saveme
+     */
+    
     private String path;
 
     private FileSystemEntries(String path) {
