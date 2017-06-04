5f8bad6c36f3ed6c7d4558646aa63d7e7fec237f
inbalzukerman <inbal.zukerman@gmail.com>
2017-06-04 23:20:27 +0300

    Revert "Added pictures to the sensors simulator"
    This reverts commit 502850a1f3ffb3ed95120248991102e1d80d287b.

--- SensorsSimulator/src/main/java/il/ac/technion/cs/smarthouse/simulator/view/SimulatorController.java
+++ SensorsSimulator/src/main/java/il/ac/technion/cs/smarthouse/simulator/view/SimulatorController.java
@@ -83,7 +83,7 @@ public class SimulatorController implements Initializable {
             e.printStackTrace();
         }
         bedroom = loader.getController();
-        bedroom.setImageUrl("/bedroom.jpg").setLocation(Location.BEDROOM).setMainController(this);
+        bedroom.setImageUrl("/house.jpg").setLocation(Location.BEDROOM).setMainController(this);
 
         // user tab:
         loader = new FXMLLoader(this.getClass().getResource("/room_ui.fxml"));
@@ -95,7 +95,7 @@ public class SimulatorController implements Initializable {
             e.printStackTrace();
         }
         bathroom = loader.getController();
-        bathroom.setImageUrl("/bathroom.jpg").setLocation(Location.BATHROOM).setMainController(this);
+        bathroom.setImageUrl("/house.jpg").setLocation(Location.BATHROOM).setMainController(this);
 
         // user tab:
         loader = new FXMLLoader(this.getClass().getResource("/room_ui.fxml"));
@@ -107,7 +107,7 @@ public class SimulatorController implements Initializable {
             e.printStackTrace();
         }
         kitchen = loader.getController();
-        kitchen.setImageUrl("/kitchen.jpg").setLocation(Location.KITCHEN).setMainController(this);
+        kitchen.setImageUrl("/house.jpg").setLocation(Location.KITCHEN).setMainController(this);
 
         // user tab:
         loader = new FXMLLoader(this.getClass().getResource("/room_ui.fxml"));
@@ -119,7 +119,7 @@ public class SimulatorController implements Initializable {
             e.printStackTrace();
         }
         livingroom = loader.getController();
-        livingroom.setImageUrl("/livingroom.jpg").setLocation(Location.LIVINGROOM).setMainController(this);
+        livingroom.setImageUrl("/house.jpg").setLocation(Location.LIVINGROOM).setMainController(this);
 
         loadSensorList();
     }
