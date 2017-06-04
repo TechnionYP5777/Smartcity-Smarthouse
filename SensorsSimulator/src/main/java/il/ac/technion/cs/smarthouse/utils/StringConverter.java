9a07326a8e771b444696bb518f7ec2bbe42b8eda
inbalzukerman <inbal.zukerman@gmail.com>
2017-06-04 23:23:24 +0300

    Revert "Remove some un-needed files"
    This reverts commit 1c86bf7f071065ebd33c7789104e5942fdcc4b52.

---
+++ SensorsSimulator/src/main/java/il/ac/technion/cs/smarthouse/utils/StringConverter.java
@@ -0,0 +1,41 @@
+package il.ac.technion.cs.smarthouse.utils;
+
+import java.beans.PropertyEditor;
+import java.beans.PropertyEditorManager;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * A class for easy String conversions
+ * 
+ * @author RON
+ * @since 30-05-2017
+ */
+public class StringConverter {
+    private static Logger log = LoggerFactory.getLogger(StringConverter.class);
+
+    /**
+     * Convert a string into any class
+     * 
+     * @param targetType
+     * @param text
+     * @return the new converted Object
+     */
+    public static Object convert(final Class<?> targetType, final String text) {
+        if (targetType.isPrimitive()) {
+            if (text == null)
+                return targetType.equals(Character.TYPE) ? '\0' : targetType.equals(Boolean.TYPE) ? false : 0;
+            if (targetType.equals(Character.TYPE))
+                return text.charAt(0);
+        }
+
+        final PropertyEditor editor = PropertyEditorManager.findEditor(targetType);
+        if (editor == null) {
+            log.warn("Converting an uknown Object to null: [targetType: " + targetType.getCanonicalName() + "]");
+            return null;
+        }
+        editor.setAsText(text);
+        return editor.getValue();
+    }
+}
