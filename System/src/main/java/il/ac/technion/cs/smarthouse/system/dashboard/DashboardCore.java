c1ea29971ba52cc96d2653df8b948bdfbd037b04
Elia Traore <set@campus.technion.ac.il>
2017-06-14 02:11:02 +0300

    Dashboard dev: dashboard as a core (part 2) the core have been updated to allow loading from cloud. (this partly implements the last required feature in the issue) Issue #181

---
+++ System/src/main/java/il/ac/technion/cs/smarthouse/system/dashboard/DashboardCore.java
@@ -0,0 +1,95 @@
+package il.ac.technion.cs.smarthouse.system.dashboard;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Optional;
+import java.util.function.Consumer;
+import java.util.function.Function;
+
+import il.ac.technion.cs.smarthouse.system.SystemCore;
+import il.ac.technion.cs.smarthouse.system.cores.ChildCore;
+import il.ac.technion.cs.smarthouse.system.dashboard.widget.BasicWidget;
+import il.ac.technion.cs.smarthouse.system.file_system.FileSystem;
+
+public class DashboardCore extends ChildCore {
+    private class wInfo {
+        WidgetType type;
+        InfoCollector info;
+        Double size;
+        
+        public wInfo(WidgetType type, InfoCollector info, Double size) {
+            this.type = type;
+            this.info = info;
+            this.size = size;
+        }
+
+        public wInfo(BasicWidget widget) {
+            this(widget.getType(), widget.getInitalInfo(), widget.getTileSize());
+        }
+
+        public Widget getRepresentedWidget(){
+            return new Widget(type, info, size);
+        }
+    }
+    
+    public class Widget {
+        private BasicWidget widget;
+        private String id;
+        
+        Widget(WidgetType type, InfoCollector pathsInfo, Double size) {
+            widget = type.createWidget(size, pathsInfo);
+            widget.updateAutomaticallyFrom(fileSystem);
+        }
+        
+        public void addToDashboard(){
+            id = DashboardCore.this.widgetPresenter.apply(widget);  //update front
+            DashboardCore.this.registerWidget(id, widget);          //update end
+        }
+        
+        public void removeFromDashboard(){
+            Optional.ofNullable(id).ifPresent(i -> {
+                DashboardCore.this.widgetHider.accept(i);           //update front
+                DashboardCore.this.deregisterWidget(i);             //update end
+            });
+        }
+    }
+    
+    FileSystem fileSystem;
+    Function<BasicWidget,String> widgetPresenter;
+    Consumer<String> widgetHider;
+    Map<String, wInfo> widgetsInfo = new HashMap<>();
+    
+    public DashboardCore(SystemCore systemCore) {
+        super(systemCore); 
+        fileSystem = systemCore.getFileSystem();
+    }
+    
+    @Override
+    public void populate(final String jsonString) throws Exception {
+        super.populate(jsonString);
+        widgetsInfo.values().forEach(winfo -> winfo.getRepresentedWidget().addToDashboard());
+    }
+    
+    public Widget createWidget(WidgetType type, InfoCollector pathsInfo, Double size){
+        return new Widget(type, pathsInfo, size);
+    }
+    
+    //-------------------front-end setters------------------------------
+    public void setWidgetPresenter(Function<BasicWidget,String> presenter){
+        widgetPresenter = presenter;
+    }
+    
+    public void setWidgetHider(Consumer<String> hider){
+        widgetHider = hider;
+    }
+    
+    //-------------------back-end offered "api"-------------------------
+    public void registerWidget(String id, BasicWidget widget){
+        widgetsInfo.put(id, new wInfo(widget));
+    }
+    
+    public void deregisterWidget(String id){
+        widgetsInfo.remove(id);
+    }
+
+}
