05a90cb845b23ad662830a40bbbe5fb7a16d2c36
Ron Gatenio <ron.gatenio@gmail.com>
2017-06-13 03:19:43 +0300

    Modified the AppComboboxField to extend ComboBox instead of HBox #184

--- System/src/main/java/il/ac/technion/cs/smarthouse/javafx_elements/AppComboBoxField.java
+++ System/src/main/java/il/ac/technion/cs/smarthouse/javafx_elements/AppComboBoxField.java
@@ -6,17 +6,17 @@ import javafx.scene.control.ComboBox;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.Priority;
 
-public class AppComboBoxField<T> extends HBox {
+public class AppComboBoxField<T> extends ComboBox<T> {
     @SafeVarargs
     public AppComboBoxField(Consumer<T> onChangeFunction, T initialValue, T... comboOptions) {
-        ComboBox<T> b = new ComboBox<>();
-        b.getItems().addAll(comboOptions);
-        b.setOnAction(e -> onChangeFunction.accept(b.getValue()));
-        b.setValue(initialValue);
-        b.setMaxWidth(Double.MAX_VALUE);
+        getItems().addAll(comboOptions);
+        setOnAction(e -> onChangeFunction.accept(getValue()));
+        if (initialValue != null)
+            setValue(initialValue);
+        else if (comboOptions.length > 0)
+            setValue(comboOptions[0]);
+        setMaxWidth(Double.MAX_VALUE);
         
-        this.getChildren().add(b);
-        
-        setHgrow(b, Priority.ALWAYS);
+        HBox.setHgrow(this, Priority.ALWAYS);
     }
 }
