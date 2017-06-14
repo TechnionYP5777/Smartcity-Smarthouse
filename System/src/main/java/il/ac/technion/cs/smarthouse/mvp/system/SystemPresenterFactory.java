bb6fba41f53127a854037d2f4a86f461898fcb4e
Ron Gatenio <ron.gatenio@gmail.com>
2017-06-14 20:35:17 +0300

    Moved the failure detector to a different class

--- System/src/main/java/il/ac/technion/cs/smarthouse/mvp/system/SystemPresenterFactory.java
+++ System/src/main/java/il/ac/technion/cs/smarthouse/mvp/system/SystemPresenterFactory.java
@@ -1,7 +1,5 @@
 package il.ac.technion.cs.smarthouse.mvp.system;
 
-import java.io.PrintWriter;
-import java.io.StringWriter;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.function.BiConsumer;
@@ -11,27 +9,11 @@ import org.slf4j.LoggerFactory;
 
 import il.ac.technion.cs.smarthouse.developers_api.SmarthouseApplication;
 import il.ac.technion.cs.smarthouse.system.SystemCore;
+import il.ac.technion.cs.smarthouse.system.SystemFailureDetector;
 import il.ac.technion.cs.smarthouse.system.applications.installer.ApplicationPath;
 import il.ac.technion.cs.smarthouse.system.applications.installer.ApplicationPath.PathType;
-import il.ac.technion.cs.smarthouse.utils.Communicate;
 import il.ac.technion.cs.smarthouse.utils.JavaFxHelper;
 import il.ac.technion.cs.smarthouse.utils.Tuple;
-import javafx.geometry.Insets;
-import javafx.geometry.Pos;
-import javafx.scene.Scene;
-import javafx.scene.control.Alert;
-import javafx.scene.control.Alert.AlertType;
-import javafx.scene.control.Button;
-import javafx.scene.control.ButtonBar;
-import javafx.scene.control.ButtonType;
-import javafx.scene.control.TextArea;
-import javafx.scene.layout.HBox;
-import javafx.scene.layout.Pane;
-import javafx.scene.layout.Priority;
-import javafx.scene.layout.VBox;
-import javafx.scene.text.Text;
-import javafx.stage.Modality;
-import javafx.stage.Stage;
 
 /**
  * A factory for {@link SystemPresenter}
@@ -52,7 +34,7 @@ public class SystemPresenterFactory {
     private boolean view_enableView = true;
     private List<Runnable> view_onCloseListeners = new ArrayList<>();
     private boolean view_openOnNewStage = true;
-    boolean disableFailureDetector = false;
+    private boolean disableFailureDetector;
 
     public SystemPresenterFactory addFileSystemSubscriber(final BiConsumer<String, Object> eventHandler,
                     final String... path) {
@@ -110,54 +92,7 @@ public class SystemPresenterFactory {
         if (disableFailureDetector)
             return;
 
-        Thread.setDefaultUncaughtExceptionHandler((t, e) -> {
-            final String errTxt = "Uncaught exception from thread [" + t.getName() + "]\n\nException:\n" + e.toString();
-            log.error(errTxt, e);
-            if (JavaFxHelper.isJavaFxThreadStarted())
-                JavaFxHelper.surroundConsumerWithFx(p -> {
-                    ButtonType reportType = new ButtonType("Send Report", ButtonBar.ButtonData.LEFT);
-                    Alert a = new Alert(AlertType.ERROR, errTxt, reportType, ButtonType.CLOSE);
-                    a.setTitle("Uncaught exception");
-                    a.showAndWait().ifPresent(response -> {
-                        if (response == reportType) {
-                            Stage dialogStage = new Stage();
-                            dialogStage.initModality(Modality.WINDOW_MODAL);
-                            TextArea userInput = new TextArea();
-                            Text text = new Text(
-                                            "Please describe what happened when the error occurred (optional):");
-                            Button reportButton = new Button("Send");
-                            reportButton.setOnMouseClicked(event -> {
-                                String input = userInput.getText();
-                                StringWriter sw = new StringWriter();
-                                PrintWriter pw = new PrintWriter(sw);
-                                e.printStackTrace(pw);
-                                Communicate.throughEmailFromHere("smarthouse5777@gmail.com",
-                                                "We got new report...\n\n" + errTxt + "\n\n"
-                                                                + (input == null || "".equals(input)
-                                                                                ? "The user did not add a description of the error."
-                                                                                : "User Description:\n" + input)  + "\n\nStack Trace:\n" +
-                                                                                sw.toString());
-                                ((Stage) reportButton.getScene().getWindow()).close();
-                            });
-                            Pane pane1 = new Pane();
-                            Pane pane2 = new Pane();
-                            HBox hbox1 = new HBox(text, pane1);
-                            hbox1.setPadding(new Insets(0, 0, 5, 0));
-                            HBox hbox2 = new HBox(pane2, reportButton);
-                            hbox2.setPadding(new Insets(5, 0, 0, 0));
-                            HBox.setHgrow(pane1, Priority.ALWAYS);
-                            HBox.setHgrow(pane2, Priority.ALWAYS);
-                            VBox vbox = new VBox(hbox1, userInput, hbox2);
-                            VBox.setVgrow(userInput, Priority.ALWAYS);
-                            vbox.setPadding(new Insets(15));
-
-                            dialogStage.setScene(new Scene(vbox));
-                            dialogStage.showAndWait();
-                        }
-                        System.exit(-1);
-                    });
-                }).accept(null);
-        });
+        SystemFailureDetector.enable();
     }
 
     public SystemPresenter build() {
