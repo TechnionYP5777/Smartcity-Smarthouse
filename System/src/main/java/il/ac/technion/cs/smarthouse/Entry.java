fb1b00cb976bdbe9b730afb07522ccc940119bcf
Ron Gatenio <ron.gatenio@gmail.com>
2017-06-19 01:37:19 +0300

    Entry modified

--- System/src/main/java/il/ac/technion/cs/smarthouse/Entry.java
+++ System/src/main/java/il/ac/technion/cs/smarthouse/Entry.java
@@ -9,7 +9,10 @@ import il.ac.technion.cs.smarthouse.utils.JavaFxHelper;
 public class Entry {
 
     public static void main(final String[] args) {
-        new SystemPresenterFactory().setUseCloudServer(false).setRegularFileSystemListeners(true).build();
+        new SystemPresenterFactory()
+                        .setUseCloudServer(false) // false for now, because there is only one instance of the cloud server
+                        .enableLocalDatabase(true)
+                        .build();
         JavaFxHelper.startGui(new SosSensorSimulator());
         JavaFxHelper.startGui(new StoveSensorSimulator());
         JavaFxHelper.startGui(new VitalsSensorSimulator());
