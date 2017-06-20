d1c1bc99e58ed72daebded8af71a8dc5d20b4f84
inbalzukerman <inbal.zukerman@gmail.com>
2017-06-20 22:37:07 +0300

    Calculator Application
    A brand new calculator application is added, allowing us to test the error handling in our system by creating a real divide by zero exception.
    
    Issue: #197

---
+++ Applications/Calculator/src/main/java/il/ac/technion/cs/smarthouse/applications/calculator/CalculatorGUI.java
@@ -0,0 +1,27 @@
+package il.ac.technion.cs.smarthouse.applications.calculator;
+
+import il.ac.technion.cs.smarthouse.developers_api.SmarthouseApplication;
+import il.ac.technion.cs.smarthouse.developers_api.application_builder.GuiBinderObject;
+
+/**
+ * 
+ * @author sinbalzu
+ *
+ */
+public class CalculatorGUI extends SmarthouseApplication {
+
+	public static void main(String[] args) throws Exception {
+		launch();
+	}
+
+	@Override
+	public void onLoad() throws Exception {
+		getAppBuilder().getCustomRegionBuilder().add("calculator_ui.fxml", new GuiBinderObject<>());
+
+	}
+
+	@Override
+	public String getApplicationName() {
+		return "Calculator Application";
+	}
+}
