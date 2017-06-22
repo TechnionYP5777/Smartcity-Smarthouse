212b1898a9d596c0326706b8e606910d4d532278
Elia Traore <set@campus.technion.ac.il>
2017-06-22 23:29:56 +0300

    refactoring the thread class out to allow compertable msg generation #195

---
+++ Common/src/main/java/il/ac/technion/cs/smarthouse/sensors/simulator/streaming_threads/MsgStreamerThread.java
@@ -0,0 +1,50 @@
+/**
+ * 
+ */
+package il.ac.technion.cs.smarthouse.sensors.simulator.streaming_threads;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import il.ac.technion.cs.smarthouse.sensors.simulator.GenericSensor;
+
+/**
+ * @author Elia Traore
+ * @since Jun 22, 2017
+ */
+
+@SuppressWarnings("rawtypes")
+public abstract class MsgStreamerThread<D> extends Thread {
+	private static Logger log = LoggerFactory.getLogger(MsgStreamerThread.class);
+	
+	GenericSensor sensor;
+	Integer streamingInterval;
+	private Boolean keepStreaming = true;
+
+	protected MsgStreamerThread(GenericSensor sensor, Integer streamingInterval) {
+		this.sensor = sensor; 
+		this.streamingInterval = streamingInterval;
+	}
+
+	@Override
+	public void interrupt() {
+		keepStreaming = false;
+		log.debug("streamer was interruped. Stopping.");
+		super.interrupt();
+	}
+
+	@Override
+	public void run() {
+		while (keepStreaming) {
+			send();
+			try {
+				Thread.sleep(streamingInterval);
+			} catch (InterruptedException e) {
+				keepStreaming = false;
+			}
+		}
+	}
+
+
+	abstract void send();
+}
