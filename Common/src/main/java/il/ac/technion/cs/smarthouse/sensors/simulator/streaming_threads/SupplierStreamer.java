f49a5e676ca3f7e132b13767a58117d37af5b0ef
Elia Traore <set@campus.technion.ac.il>
2017-06-22 23:35:41 +0300

    added streamer bu supplier and inital support through builder #195

---
+++ Common/src/main/java/il/ac/technion/cs/smarthouse/sensors/simulator/streaming_threads/SupplierStreamer.java
@@ -0,0 +1,34 @@
+/**
+ * 
+ */
+package il.ac.technion.cs.smarthouse.sensors.simulator.streaming_threads;
+
+import java.util.Map;
+
+import com.google.common.base.Supplier;
+
+import il.ac.technion.cs.smarthouse.sensors.simulator.GenericSensor;
+
+/**
+ * @author Elia Traore
+ * @since Jun 22, 2017
+ */
+public class SupplierStreamer extends MsgStreamerThread<Supplier<Map<String, Object>>>{
+
+	private Supplier<Map<String, Object>> msgGenerator;
+
+	public SupplierStreamer(GenericSensor sensor, Integer streamingInterval, Supplier<Map<String, Object>> msgGenerator) {
+		super(sensor, streamingInterval);
+		this.msgGenerator = msgGenerator;
+	}
+
+	/* (non-Javadoc)
+	 * @see il.ac.technion.cs.smarthouse.sensors.simulator.streaming_threads.MsgStreamerThread#send()
+	 */
+	@Override
+	void send() {
+		sensor.sendMessage(msgGenerator.get());
+		
+	}
+
+}
