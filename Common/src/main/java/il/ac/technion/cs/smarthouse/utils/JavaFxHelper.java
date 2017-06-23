520f8b6b05e42721a3fe87add1b4841b15e74cf9
Ron Gatenio <ron.gatenio@gmail.com>
2017-06-23 04:07:50 +0300

    Added a close all stages function to javaFxHelper

--- Common/src/main/java/il/ac/technion/cs/smarthouse/utils/JavaFxHelper.java
+++ Common/src/main/java/il/ac/technion/cs/smarthouse/utils/JavaFxHelper.java
@@ -1,5 +1,7 @@
 package il.ac.technion.cs.smarthouse.utils;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.function.Consumer;
 
 import org.slf4j.Logger;
@@ -20,136 +22,120 @@ import javafx.stage.Stage;
  * @since 11-05-2017
  */
 public enum JavaFxHelper {
-	;
+    ;
 
-	private static Logger log = LoggerFactory.getLogger(JavaFxHelper.class);
+    private static Logger log = LoggerFactory.getLogger(JavaFxHelper.class);
+    private static final List<Stage> openStages = new ArrayList<>();
 
-	/**
-	 * Surrounds the given function with a Platform.runLater
-	 * 
-	 * @param functionToRun
-	 *            - the Consumer to surround
-	 * @return the modified consumer
-	 */
-	public static <T> Consumer<T> surroundConsumerWithFx(final Consumer<T> functionToRun) {
-		return x -> {
-			if (Platform.isFxApplicationThread())
-				functionToRun.accept(x);
-			else
-				try {
-					Platform.runLater(() -> functionToRun.accept(x));
-				} catch (final IllegalStateException __) {
-					new Thread(() -> functionToRun.accept(x)).start();
-				}
-		};
-	}
+    /**
+     * Surrounds the given function with a Platform.runLater
+     * 
+     * @param functionToRun
+     *            - the Consumer to surround
+     * @return the modified consumer
+     */
+    public static <T> Consumer<T> surroundConsumerWithFx(final Consumer<T> functionToRun) {
+        return x -> {
+            if (Platform.isFxApplicationThread())
+                functionToRun.accept(x);
+            else
+                try {
+                    Platform.runLater(() -> functionToRun.accept(x));
+                } catch (final IllegalStateException __) {
+                    new Thread(() -> functionToRun.accept(x)).start();
+                }
+        };
+    }
 
-	@Deprecated
-	public static void runOnFx(final Runnable functionToRun, final boolean wait) {
-		final BoolLatch isDone = new BoolLatch();
+    public static void placeNodeInPane(final Node n, final Pane parent) {
+        AnchorPane.setTopAnchor(n, 0.0);
+        AnchorPane.setRightAnchor(n, 0.0);
+        AnchorPane.setLeftAnchor(n, 0.0);
+        AnchorPane.setBottomAnchor(n, 0.0);
 
-		if (!isJavaFxThreadStarted())
-			initJavaFxThread();
+        parent.getChildren().setAll(n);
+    }
 
-		Platform.runLater(() -> {
-			functionToRun.run();
-			isDone.setTrueAndRelease();
+    /**
+     * Checks if the JavaFx thread was initiated
+     * 
+     * @return true if the JavaFx thread was initiated, or false otherwise
+     */
+    public static boolean isJavaFxThreadStarted() {
+        try {
+            Platform.runLater(() -> {});
+        } catch (final IllegalStateException __) {
+            return false;
+        }
+        return true;
+    }
 
-		});
+    /**
+     * initializes the javaFx toolkit
+     */
+    public static void initJavaFxThread() {
+        // PlatformImpl.startup(()->{});
+        new JFXPanel();
+    }
 
-		if (wait)
-			isDone.blockUntilTrue();
-	}
+    /**
+     * Closes all stages that were opened by
+     * {@link #startGui(Application, boolean)}
+     */
+    public static void closeAllOpenedStages() {
+        openStages.forEach(s -> s.close());
+    }
 
-	public static void placeNodeInPane(final Node n, final Pane parent) {
-		AnchorPane.setTopAnchor(n, 0.0);
-		AnchorPane.setRightAnchor(n, 0.0);
-		AnchorPane.setLeftAnchor(n, 0.0);
-		AnchorPane.setBottomAnchor(n, 0.0);
+    /**
+     * starts a {@link javafx.application.Application}
+     * 
+     * @param jfxApplication
+     *            the application
+     * @param blockUntilStarted
+     *            if true the function blocks until the application is started
+     *            (until {@link javafx.application.Application#start(Stage)}
+     *            finishes), otherwise the function returns immediately
+     * @return
+     */
+    public static <T extends Application> T startGui(final T jfxApplication, final boolean blockUntilStarted) {
+        assert jfxApplication != null;
 
-		parent.getChildren().setAll(n);
-	}
+        // init javafx thread if the thread wasn't initiated yet:
+        if (!isJavaFxThreadStarted())
+            initJavaFxThread();
 
-	/**
-	 * Checks if the JavaFx thread was initiated
-	 * 
-	 * @return true if the JavaFx thread was initiated, or false otherwise
-	 */
-	public static boolean isJavaFxThreadStarted() {
-		try {
-			Platform.runLater(() -> {
-			});
-		} catch (final IllegalStateException __) {
-			return false;
-		}
-		return true;
-	}
+        // make a lock that will wait for jfxApplication to start:
+        final BoolLatch guiStarted = new BoolLatch();
 
-	/**
-	 * initializes the javaFx toolkit
-	 */
-	public static void initJavaFxThread() {
-		// PlatformImpl.startup(()->{});
-		new JFXPanel();
-	}
+        try {
+            Platform.runLater(() -> {
+                try {
+                    Stage s = new Stage();
+                    jfxApplication.start(s);
+                    openStages.add(s);
+                    guiStarted.setTrueAndRelease();
+                } catch (final Exception e) {
+                    log.error("couldn't start " + jfxApplication.getClass().getName(), e);
+                }
+            });
+        } catch (final IllegalStateException ¢) {
+            log.error("Platform.runLater failed! This shouldn't happen!", ¢);
+        }
 
-	/**
-	 * starts a {@link javafx.application.Application} and blocks until it is
-	 * loaded
-	 * 
-	 * @param jfxApplication
-	 *            the application
-	 * @param args
-	 */
+        if (blockUntilStarted)
+            guiStarted.blockUntilTrue();
 
-	/**
-	 * starts a {@link javafx.application.Application}
-	 * 
-	 * @param jfxApplication
-	 *            the application
-	 * @param blockUntilStarted
-	 *            if true the function blocks until the application is started
-	 *            (until {@link javafx.application.Application#start(Stage)}
-	 *            finishes), otherwise the function returns immediately
-	 * @return
-	 */
-	public static <T extends Application> T startGui(final T jfxApplication, final boolean blockUntilStarted) {
-		assert jfxApplication != null;
+        return jfxApplication;
+    }
 
-		// init javafx thread if the thread wasn't initiated yet:
-		if (!isJavaFxThreadStarted())
-			initJavaFxThread();
-
-		// make a lock that will wait for jfxApplication to start:
-		final BoolLatch guiStarted = new BoolLatch();
-
-		try {
-			Platform.runLater(() -> {
-				try {
-					jfxApplication.start(new Stage());
-					guiStarted.setTrueAndRelease();
-				} catch (final Exception e) {
-					log.error("couldn't start " + jfxApplication.getClass().getName(), e);
-				}
-			});
-		} catch (final IllegalStateException ¢) {
-			log.error("Platform.runLater failed! This shouldn't happen!", ¢);
-		}
-
-		if (blockUntilStarted)
-			guiStarted.blockUntilTrue();
-
-		return jfxApplication;
-	}
-
-	/**
-	 * same as {@link JavaFxHelper#startGui(Application, boolean)} with
-	 * blockUntilStarted = true
-	 * 
-	 * @param jfxApplication
-	 * @return
-	 */
-	public static <T extends Application> T startGui(final T jfxApplication) {
-		return startGui(jfxApplication, true);
-	}
+    /**
+     * same as {@link JavaFxHelper#startGui(Application, boolean)} with
+     * blockUntilStarted = true
+     * 
+     * @param jfxApplication
+     * @return
+     */
+    public static <T extends Application> T startGui(final T jfxApplication) {
+        return startGui(jfxApplication, true);
+    }
 }
