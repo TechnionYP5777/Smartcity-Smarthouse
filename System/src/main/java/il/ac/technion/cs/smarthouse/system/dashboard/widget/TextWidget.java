9601d4a60ac0a9fd20b9c425cf623b005021de48
inbalzukerman <inbal.zukerman@gmail.com>
2017-06-23 16:09:05 +0300

    TextWidget - a new widget to present strings (and more) is now added. #214

---
+++ System/src/main/java/il/ac/technion/cs/smarthouse/system/dashboard/widget/TextWidget.java
@@ -0,0 +1,59 @@
+package il.ac.technion.cs.smarthouse.system.dashboard.widget;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import il.ac.technion.cs.smarthouse.system.dashboard.InfoCollector;
+import il.ac.technion.cs.smarthouse.system.dashboard.WidgetType;
+import il.ac.technion.cs.smarthouse.system.file_system.FileSystem;
+import il.ac.technion.cs.smarthouse.system.file_system.FileSystemEntries;
+import il.ac.technion.cs.smarthouse.system.file_system.PathBuilder;
+import javafx.application.Platform;
+import javafx.scene.control.Label;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
+
+/**
+ * 
+ * @author Inbal Zukerman
+ * @since 23-06-2017
+ */
+public class TextWidget extends BasicWidget {
+
+    private static Logger log = LoggerFactory.getLogger(TextWidget.class);
+    public Label dataText = new Label("Data");
+
+    public TextWidget(final WidgetType type, final Double tileSize, final InfoCollector data) {
+        super(type, tileSize, data);
+
+        if (data.getInfoEntries().isEmpty()) {
+            log.warn(type + " widget is not supposed to be initialized with no data entries. The widget will not update.");
+            return;
+        }
+
+        dataText.setTextFill(Color.WHITE);
+        dataText.setFont(new Font("Cambria", 32));
+        builder.graphic(dataText);
+
+    }
+
+    @Override
+    protected void updateAutomaticallyFrom(final FileSystem s, final String path) {
+        s.subscribe((rPath, sData) -> update(sData, path), FileSystemEntries.SENSORS_DATA.buildPath(path));
+    }
+
+    public void update(final Object value, final String key) {
+
+        getTile().setText(key.replace(PathBuilder.DELIMITER, " "));
+
+        if (value != null)
+            Platform.runLater(() -> dataText.setText(value.toString()));
+
+    }
+
+    @Override
+    public String getTitle() {
+        return "Text Widget";
+    }
+
+}
