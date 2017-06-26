f402e5f55c8482dc58efa20a1db2cf335daa340a
Elia Traore <set@campus.technion.ac.il>
2017-06-26 06:12:50 +0300

    added ability to initialize SsS Gui with custom simulator #221

--- SensorsSimulator/src/main/java/il/ac/technion/cs/smarthouse/DeveloperSimulator/DeveloperSimulatorGui.java
+++ SensorsSimulator/src/main/java/il/ac/technion/cs/smarthouse/DeveloperSimulator/DeveloperSimulatorGui.java
@@ -7,14 +7,25 @@ import javafx.stage.Stage;
 
 public class DeveloperSimulatorGui extends Application{
 
+	private SensorsSimulator simulator;
+
 	@Override
 	public void start(Stage s) throws Exception {
-		final Scene scene = new Scene(DeveloperSimulatorController.createRootController(getClass().getResource("/main_window.fxml"), new SensorsSimulator()).getRootViewNode());
+		final Scene scene = new Scene(DeveloperSimulatorController.createRootController(
+											getClass().getResource("/main_window.fxml"), 
+											(simulator!= null) ? simulator : new SensorsSimulator()
+										).getRootViewNode()
+									);
         s.setTitle("Sensor Simulator");
         s.setScene(scene);
         s.setResizable(false);
         s.show();
 		
+	}
+	
+	public DeveloperSimulatorGui setSimulator(SensorsSimulator s){
+		this.simulator = s;
+		return this;
 	}
 	
     public static void main(final String[] args) {
