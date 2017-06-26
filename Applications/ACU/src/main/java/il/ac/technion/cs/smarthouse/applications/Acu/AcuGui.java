fdcdd61bf293479645e11442de61f791f93fc6a2
Elia Traore <set@campus.technion.ac.il>
2017-06-26 04:55:37 +0300

    refactor of the classes for clearity #195

--- Applications/ACU/src/main/java/il/ac/technion/cs/smarthouse/applications/Acu/AcuGui.java
+++ Applications/ACU/src/main/java/il/ac/technion/cs/smarthouse/applications/Acu/AcuGui.java
@@ -3,6 +3,7 @@
  */
 package il.ac.technion.cs.smarthouse.applications.Acu;
 
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -25,59 +26,90 @@ import il.ac.technion.cs.smarthouse.system.services.sensors_service.SystemPath;
  */
 public class AcuGui extends SmarthouseApplication{
 	static Simulation simulation = new Simulation(30,//summer sucks
-										"Roy's Man Cave", "Ron's Home Cinema", "Yarden's AI lab", "Inbal's sleeping chamber");
-	final static String setTempPath = Simulation.getPath(PathType.INSTRUCTION_RECEIVING,Simulation.defaultTempSuffix);
-	final static String setStatePath = Simulation.getPath(PathType.INSTRUCTION_RECEIVING,Simulation.stateSuffix);
-	final static String getTempPath = Simulation.getPath(PathType.INFO_SENDING,Simulation.tempSuffix);
+										"Roy's Man Cave", "Ron's Home Cinema");
+//										, "Yarden's AI lab", "Inbal's dogs kennel");
+	static final String setTempPath = Simulation.getPath(PathType.INSTRUCTION_RECEIVING, Simulation.defaultTempSuffix);
+	static final String setStatePath = Simulation.getPath(PathType.INSTRUCTION_RECEIVING, Simulation.stateSuffix);
+	static final String getTempPath = Simulation.getPath(PathType.INFO_SENDING, Simulation.tempSuffix);
 	
+	static final String ON="on", OFF="off";
+	
+	AcuAction prevAction;
 	public static void main(String[] args) throws Exception {
-		simulation.start();
-		launch();
+		launch(simulation.getSimulator(), false);
 	}
 
 	/* (non-Javadoc)
 	 * @see il.ac.technion.cs.smarthouse.developers_api.SmarthouseApplication#onLoad()
 	 */
+	/**
+	 * [[SuppressWarningsSpartan]]
+	 */
 	@Override
 	public void onLoad() throws Exception {
 		final GuiBinderObject<Integer> currentTemp = new GuiBinderObject<>(20);
-		final GuiBinderObject<Integer> wantedTemp = new GuiBinderObject<>(20);
+		final GuiBinderObject<Integer> wantedTemp = new GuiBinderObject<>(18);
+		final GuiBinderObject<String> AcuState = new GuiBinderObject<>(OFF);
 		
 		final SensorApi<ACUSensor> sensor = super.<SensorsService>getService(ServiceType.SENSORS_SERVICE)
-				.getSensor(Simulation.commname, ACUSensor.class);
+																	.getSensor(Simulation.commname, ACUSensor.class);
 		sensor.subscribe(data -> {
+				//update GUI
+				currentTemp.setData(data.getTemp());
+				AcuState.setData(data.isOn() ? ON : OFF);
+				
+				//instruct sensor
 				Integer want = wantedTemp.getData(100), have = currentTemp.getData(0);
-				if(want < have)
-				sensor.instruct(AcuAction.HOTTER+"", setStatePath);
-				if(want > have)
-				sensor.instruct(AcuAction.COLDER+"", setStatePath);
-				if(want == have)
-				sensor.instruct(AcuAction.STOP+"", setStatePath);
+				AcuAction nextAction = (want < have) ? AcuAction.HOTTER : (want > have)? AcuAction.COLDER: AcuAction.STOP;
+				if(!nextAction.equals(prevAction)){
+					sensor.instruct(nextAction+"", setStatePath);
+					prevAction = nextAction;
+				}
 		});
 		
-		wantedTemp.addOnDataChangedListener(
-				desiredT -> desiredT.getDataAsOptional().ifPresent(t->sensor.instruct(t+"", setTempPath)));
+		sensor.instruct(wantedTemp.getData()+0.1+"", setTempPath); //init so the simulation and the gui are on the same page
 		
-		getAppBuilder().getStatusRegionBuilder().addStatusField("Current Temperature:", currentTemp);
+		wantedTemp.addOnDataChangedListener(
+				desiredT -> desiredT.getDataAsOptional().ifPresent(t->sensor.instruct(t+0.2+"", setTempPath)));
+		
+		getAppBuilder().getStatusRegionBuilder()
+											.addStatusField("Current temperature in controlled location:", currentTemp)
+											.addStatusField("The air conditioner in controlled location is", AcuState);
+		
 		getAppBuilder().getConfigurationsRegionBuilder()
 											.addIntegerInputField("Desired Temperature:", wantedTemp)
-											.addSensorAliasSelectionField("The Controlled location:", sensor);
-		
+											.addSensorAliasSelectionField("The Controlled location:", sensor, 
+													(oldA, newA)->{
+														super.<SensorsService>getService(ServiceType.SENSORS_SERVICE)
+														.getSensor(Simulation.commname, ACUSensor.class, oldA)
+														.instruct(AcuAction.STOP+"", setStatePath);
+														
+														wantedTemp.getDataAsOptional()
+																	.ifPresent(t->
+															super.<SensorsService>getService(ServiceType.SENSORS_SERVICE)
+															.getSensor(Simulation.commname, ACUSensor.class, newA)
+															.instruct(t+0.3+"", setTempPath)
+														);
+													}
+													);
+
 		simulation.getAliases()
-					.stream()
-					.map(alias -> super.<SensorsService>getService(ServiceType.SENSORS_SERVICE)
-										.getSensor(Simulation.commname, ACUSensor.class, alias))
-					.forEach(currSensor -> {
+					.forEach( alias-> {
+						SensorApi<ACUSensor> currSensor = super.<SensorsService>getService(ServiceType.SENSORS_SERVICE)
+															.getSensor(Simulation.commname, ACUSensor.class, alias);
 						getAppBuilder().getWidgetsRegionBuilder()
-										.addWidget(WidgetType.LINES_GRAPH, 
-													new InfoCollector().addInfoEntry(getTempPath, sensor.getSensorAlias()), 
+										.addWidget(WidgetType.PROGRESS_LINE_GRAPH, 
+													new InfoCollector()
+														.addInfoEntry(getTempPath, "temperature")
+														.setTitle("At "+alias), 
 													currSensor, 
 													acu -> {
 														Map<String,Object> data = new HashMap<>();
 														data.put(getTempPath, acu.getTemp());
 														return data;
 													});
-					});
+						}
+					);
 		
 	}
 
