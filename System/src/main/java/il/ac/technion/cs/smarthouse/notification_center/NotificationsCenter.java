7da2d6292763a7f4626b9ac2680d2820706ac925
inbalzukerman <inbal.zukerman@gmail.com>
2017-06-30 20:57:54 +0300

    Code review- notification center #202

--- System/src/main/java/il/ac/technion/cs/smarthouse/notification_center/NotificationsCenter.java
+++ System/src/main/java/il/ac/technion/cs/smarthouse/notification_center/NotificationsCenter.java
@@ -9,31 +9,72 @@ import eu.hansolo.enzo.notification.*;
 import il.ac.technion.cs.smarthouse.system.services.alerts_service.EmergencyLevel;
 import il.ac.technion.cs.smarthouse.utils.JavaFxHelper;
 
+/**
+ * @author RON
+ * @since 14-06-2017
+ * 
+ *        The notification center manages the notifications to the user while
+ *        using the house system GUI
+ */
 public enum NotificationsCenter {
     ;
     private static Logger log = LoggerFactory.getLogger(NotificationsCenter.class);
 
     private static boolean isEnabled;
 
+    /**
+     * This method sends a notification on a successful connection of a new
+     * sensor
+     * 
+     * @param sensorCommercialName
+     * @param sensorId
+     * @param sensorAlias
+     */
     public static void sendSensorConnectedNotification(final String sensorCommercialName, final String sensorId,
                     final String sensorAlias) {
         sendNotification("A new sensor has connected!", "Commercial name: " + sensorCommercialName + "\nMac id: "
                         + sensorId + "\nAlias: " + sensorAlias, Notification.INFO_ICON);
     }
 
+    /**
+     * This method sends a notification on an emergency alert
+     * 
+     * @param senderName
+     * @param message
+     * @param eLevel
+     */
     public static void sendAlertNotifications(final String senderName, final String message,
                     final EmergencyLevel eLevel) {
         sendNotification("Alert! " + senderName, message + "\n" + eLevel.toPretty(), Notification.WARNING_ICON);
     }
 
+    /**
+     * This method sends a notification on a successful installation of a new
+     * application
+     * 
+     * @param applicationName
+     */
     public static void sendNewAppInstalled(final String applicationName) {
         sendNotification(applicationName, "Installed successfully", Notification.SUCCESS_ICON);
     }
 
+    /**
+     * This method sends a notification on a failed installation of a new
+     * application
+     * 
+     * @param error
+     */
     public static void sendAppFailedToInstall(final String error) {
         sendNotification("Failed installation", error, Notification.ERROR_ICON);
     }
 
+    /**
+     * This method is responsible on showing the actual notification
+     * 
+     * @param title
+     * @param msg
+     * @param icon
+     */
     public static void sendNotification(final String title, final String msg, final Image icon) {
         if (!isEnabled)
             return;
@@ -46,10 +87,16 @@ public enum NotificationsCenter {
                             .accept(null);
     }
 
+    /**
+     * This method enables the notification center
+     */
     public static void enable() {
         isEnabled = true;
     }
 
+    /**
+     * This method closes the notifications center
+     */
     public static void close() {
         try {
             Notification.Notifier.INSTANCE.stop();
