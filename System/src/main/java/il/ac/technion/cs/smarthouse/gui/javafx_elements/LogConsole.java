cf88b27887a875f73150e51376499b0e5b85b8f9
inbalzukerman <inbal.zukerman@gmail.com>
2017-07-01 20:03:31 +0300

    Code review - LogConsole #200 #202

--- System/src/main/java/il/ac/technion/cs/smarthouse/gui/javafx_elements/LogConsole.java
+++ System/src/main/java/il/ac/technion/cs/smarthouse/gui/javafx_elements/LogConsole.java
@@ -2,17 +2,23 @@ package il.ac.technion.cs.smarthouse.gui.javafx_elements;
 
 import org.apache.log4j.WriterAppender;
 import org.apache.log4j.spi.LoggingEvent;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import javafx.application.Platform;
 import javafx.scene.control.TextArea;
 
 /**
+ * This is an appender which will be used in order to present the LOG in a GUI
+ * console
+ * 
  * @author Inbal Zukerman
  * @since 25-06-2017
  */
 public class LogConsole extends WriterAppender {
 
     private static TextArea console;
+    private static Logger log = LoggerFactory.getLogger(LogConsole.class);
 
     public static void setLogConsole(TextArea a) {
         LogConsole.console = a;
@@ -39,6 +45,7 @@ public class LogConsole extends WriterAppender {
             });
         } catch (IllegalStateException e) {
             // ignore - the relevant GUI is not initialized!
+            log.error("No relevant GUI element was initialized", e);
         }
     }
 
